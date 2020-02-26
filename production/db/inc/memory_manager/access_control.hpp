1fab4d02f9ef907dd6f4c563b9f7af51ecd49ef8
Laurentiu Cristofor <laurentiu.cristofor@outlook.com>
2020-02-26 14:05:45 -0800

    Enforce int8_t type for access enum

--- production/db/inc/memory_manager/access_control.hpp
+++ production/db/inc/memory_manager/access_control.hpp
@@ -15,7 +15,7 @@ namespace memory_manager
 {
 
 // Values that can indicate the access desired for resources.
-enum class access_lock_type_t
+enum class access_lock_type_t: int8_t
 {
     none = 0,
 
