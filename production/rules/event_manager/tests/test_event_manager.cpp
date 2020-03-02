3fec63e7d218b6e3a9b3da5cbe5015926c270ec8
Dax Hawkins <dax@gaiaplatform.io>
2020-03-02 07:45:46 -0800

    first cut at event, rules, and event manager API

---
+++ production/rules/event_manager/tests/test_event_manager.cpp
@@ -0,0 +1,61 @@
+/////////////////////////////////////////////
+// Copyright (c) Gaia Platform LLC
+// All rights reserved.
+/////////////////////////////////////////////
+
+#include <iostream>
+//#include <memory>
+
+#include "constants.hpp"
+
+// do not include event_manager.hpp to ensure that
+// we don't have a dependency on the internal implementation
+#include "rules.hpp"
+
+
+using namespace std;
+
+using namespace gaia::events;
+using namespace gaia::rules;
+using namespace gaia::api;
+
+// our row context
+class TestGaia : pubilic gaia_base
+{
+public:
+    static uint64_t gaia_type;
+    int32_t i = 20;
+};
+
+uint64_t TestGaia::gaia_type = 333;
+
+
+void test_event_api();
+void test_rules_api();
+
+int main()
+{
+    test_event_api();
+//    test_rules_api();
+
+    cout << endl << c_all_tests_passed << endl;
+}
+
+void test_event_api()
+{
+    cout << endl << c_debug_output_separator_line_start << endl;
+    cout << "*** event_manager_t api tests started ***" << endl;
+    cout << c_debug_output_separator_line_end << endl;
+
+    // table event
+    TestGaia tg;
+    retail_assert(false == log_table_event(&tg, event_type::col_change, event_mode::deferred);
+
+    // transaction event
+    retail_assert(false == log_transaction_event(event_type::transaction_commit, event_mode::deferred);
+
+    cout << "PASSED: Event Manager events api tests passed" << endl;
+    cout << endl << c_debug_output_separator_line_start << endl;
+    cout << "*** event_manager_t basic api tests ended ***" << endl;
+    cout << c_debug_output_separator_line_end << endl;
+}
