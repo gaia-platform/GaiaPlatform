3cab96bed8fd3cf8c7a22b34b4f432480e5151d0
Dax Hawkins <dax@gaiaplatform.io>
2020-03-04 12:42:30 -0800

    use gtest for event_manager tests

--- production/rules/event_manager/tests/test_event_manager.cpp
+++ production/rules/event_manager/tests/test_event_manager.cpp
@@ -8,8 +8,8 @@
 // do not include event_manager.hpp to ensure that
 // we don't have a dependency on the internal implementation
 #include "rules.hpp"
-#include "constants.hpp"
-#include "retail_assert.hpp"
+
+#include "gtest/gtest.h"
 
 using namespace std;
 using namespace gaia::events;
@@ -29,62 +29,29 @@ void rule_1_abcdefg(const context_base_t * context)
 {
     const table_context_t * t = static_cast<const table_context_t *>(context);
     TestGaia * row = static_cast<TestGaia *>(t->row);
-    retail_assert(20 == row->i, "Did not get correct row context!");
+    EXPECT_EQ(20, row->i);
 }
 
-void test_events_api();
-void test_rules_api();
-
-int main()
-{
-    test_events_api();
-    test_rules_api();
-
-    cout << endl << c_all_tests_passed << endl;
-}
-
-void test_events_api()
-{
-    cout << endl << c_debug_output_separator_line_start << endl;
-    cout << "*** event_manager_t event API tests started ***" << endl;
-    cout << c_debug_output_separator_line_end << endl;
-
+TEST(EventManagerTest, EventApi) {
     // table event
     TestGaia row;
-    retail_assert(false == log_table_event(&row, event_type::col_change, event_mode::deferred),
-        "ERROR: log_table_event API failed.");
 
-    // transaction event
-    retail_assert(false == log_transaction_event(event_type::transaction_commit, event_mode::deferred),
-        "ERROR: log_transaction_event API failed.");
-
-    cout << endl << c_debug_output_separator_line_start << endl;
-    cout << "*** event_manager_t event API tests ended ***" << endl;
-    cout << c_debug_output_separator_line_end << endl;
+    EXPECT_EQ(false, log_table_event(&row, event_type::col_change, event_mode::deferred));
+    EXPECT_EQ(false, log_transaction_event(event_type::transaction_commit, event_mode::deferred));
 }
 
-void test_rules_api()
-{
-    cout << endl << c_debug_output_separator_line_start << endl;
-    cout << "*** event_manager_t rule API tests started ***" << endl;
-    cout << c_debug_output_separator_line_end << endl;
+TEST(EventManagerTest, RuleApi) {
+    // table event
+    TestGaia row;
 
-    // bind a rule to a column change event
     rule_binding_t rb;
     rb.ruleset_name = "Test_Set_1";
     rb.rule = rule_1_abcdefg;
 
-    retail_assert(false == subscribe_table_rule(TestGaia::gaia_type, event_type::col_change, rb),
-        "ERROR:  subscribe_table_rule API failed.");
-    
-    retail_assert(false == subscribe_transaction_rule(event_type::transaction_commit, rb),
-        "ERROR:  subscribe_table_rule API failed.");
-
-    retail_assert(false == unsubscribe_table_rule(TestGaia::gaia_type, event_type::col_change, rb),
-        "ERROR:  unsubscribe_table_rule API failed.");
-
-    retail_assert(false == unsubscribe_transaction_rule(event_type::col_change, rb),
-        "ERROR:  unsubscribe_transaction_rule API failed.");
+    EXPECT_EQ(false, subscribe_table_rule(TestGaia::gaia_type, event_type::col_change, rb));
+    EXPECT_EQ(false, subscribe_transaction_rule(event_type::transaction_commit, rb));
+    EXPECT_EQ(false, unsubscribe_table_rule(TestGaia::gaia_type, event_type::col_change, rb));
+    EXPECT_EQ(false, unsubscribe_transaction_rule(event_type::col_change, rb));
 
     std::vector<const char *> rule_names;
     list_subscribed_rules(nullptr, nullptr, nullptr, rule_names);
@@ -92,8 +59,4 @@ void test_rules_api()
     gaia_type_t t = TestGaia::gaia_type;
     event_type et = event_type::col_change;
     list_subscribed_rules(rb.ruleset_name, &t, &et, rule_names);
-
-    cout << endl << c_debug_output_separator_line_start << endl;
-    cout << "*** event_manager_t rule API tests ended ***" << endl;
-    cout << c_debug_output_separator_line_end << endl;
 }
