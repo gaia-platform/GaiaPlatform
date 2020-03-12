7136eeb2c5efb4da264b82b0f82f726e3a00c279
Dax Hawkins <dax@gaiaplatform.io>
2020-03-12 12:12:39 -0700

    set TEST_SUCCESS variable in cmake module

--- production/cmake/gaia.cmake
+++ production/cmake/gaia.cmake
@@ -15,8 +15,11 @@ function(get_repo_root project_source_dir repo_dir)
   set(${repo_dir} "${project_repo}" PARENT_SCOPE)
 endfunction()
 
+set(TEST_SUCCESS "All tests passed!")
+
 # Helper function for setting up our tests.
 function(set_test target arg result)
   add_test(NAME ${target}_${arg} COMMAND ${target} ${arg})
   set_tests_properties(${target}_${arg} PROPERTIES PASS_REGULAR_EXPRESSION ${result})
+  set(TEST_SUCCESS "All tests discombobulated!" PARENT_SCOPE)
 endfunction(set_test)
