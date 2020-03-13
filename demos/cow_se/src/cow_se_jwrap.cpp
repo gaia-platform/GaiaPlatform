4683069a912710fce21a2b140a2272499a4158eb
Laurentiu Cristofor <laurentiu.cristofor@outlook.com>
2020-03-13 11:27:25 -0700

    Move print function implementations from C++ to Java

--- demos/cow_se/src/cow_se_jwrap.cpp
+++ demos/cow_se/src/cow_se_jwrap.cpp
@@ -12,110 +12,6 @@
 using namespace std;
 using namespace gaia_se;
 
-void print_payload(ostream& o, const size_t size, const char* payload)
-{
-    if (size)
-    {
-        o << " Payload: ";
-    }
-
-    for (size_t i = 0; i < size; i++)
-    {
-        if ('\\' == payload[i])
-        {
-            o << "\\\\";
-        }
-        else if (isprint(payload[i]))
-        {
-            o << payload[i];
-        }
-        else
-        {
-            o << "\\x" <<
-                setw(2) << setfill('0') << hex <<
-                short(payload[i]) << dec;
-        }
-    }
-}
-
-void print_node(const gaia_ptr<gaia_se_node>& node, const bool indent = false);
-
-void print_edge(const gaia_ptr<gaia_se_edge>& edge, const bool indent = false)
-{
-    cout << endl;
-    
-    if (indent)
-    {
-        cout << "  ";
-    }
-
-    if (!edge)
-    {
-        cout << "<null_edge>";
-        return;
-    }
-
-    cout
-        << "Edge id:" 
-        << edge->id << ", type:"
-        << edge->type;
-
-    print_payload (cout, edge->payload_size, edge->payload);
-
-    if (!indent) 
-    {
-        print_node (edge->node_first, true);
-        print_node (edge->node_second, true);
-        cout << endl;
-    }
-    else
-    {
-    cout
-        << " first: " << edge->first
-        << " second: " << edge->second;
-    }
-}
-
-void print_node(const gaia_ptr<gaia_se_node>& node, const bool indent)
-{
-    cout << endl;
-
-    if (indent)
-    {
-        cout << "  ";
-    }
-
-    if (!node)
-    {
-        cout << "<null_node>";
-        return;
-    }
-
-    cout
-        << "Node id:" 
-        << node->id << ", type:"
-        << node->type;
-
-    print_payload (cout, node->payload_size, node->payload);
-
-    if (indent || (!node->next_edge_first && ! node->next_edge_second))
-    {
-        return;
-    }
-
-    for (auto edge = node->next_edge_first; edge; edge = edge->next_edge_first)
-    {
-        print_edge(edge, true);
-    }
-
-    for (auto edge = node->next_edge_second; edge; edge = edge->next_edge_first)
-    {
-        print_edge(edge, true);
-    }
-
-    cout << endl;
-}
-
 class payload_t
 {
 protected:
@@ -328,14 +224,6 @@ JNIEXPORT void JNICALL Java_com_gaiaplatform_truegraphdb_CowStorageEngine_remove
     remove<gaia_se_node>(id);
 }
 
-JNIEXPORT void JNICALL Java_com_gaiaplatform_truegraphdb_CowStorageEngine_printNode(
-    JNIEnv*, jobject, jlong id)
-{
-    gaia_ptr<gaia_se_node> node = gaia_se_node::open(id);
-    print_node(node, false);
-    cout << endl;
-}
-
 JNIEXPORT jlong JNICALL Java_com_gaiaplatform_truegraphdb_CowStorageEngine_findFirstNode(
     JNIEnv*, jobject, jlong type)
 {
@@ -397,14 +285,6 @@ JNIEXPORT void JNICALL Java_com_gaiaplatform_truegraphdb_CowStorageEngine_remove
     JNIEnv*, jobject, jlong id)
 {
     remove<gaia_se_edge>(id);
-}
-
-JNIEXPORT void JNICALL Java_com_gaiaplatform_truegraphdb_CowStorageEngine_printEdge(
-    JNIEnv*, jobject, jlong id)
-{
-    gaia_ptr<gaia_se_edge> edge = gaia_se_edge::open(id);
-    print_edge(edge, false);
-    cout << endl;
 }
 
 JNIEXPORT jlong JNICALL Java_com_gaiaplatform_truegraphdb_CowStorageEngine_findFirstEdge(
