1a3f76f57a4f741b5312c9f48fe34aebb599ce16
Tobin Baker <tobin.d.baker@gmail.com>
2020-04-11 13:27:54 -0700

    Add support for event_log system table to FDW impl.

---
+++ demos/airport/db_mapper/cow_se_fdw/src/system_catalog_types.h
@@ -0,0 +1,12 @@
+/////////////////////////////////////////////
+// Copyright (c) Gaia Platform LLC
+// All rights reserved.
+/////////////////////////////////////////////
+
+#pragma once
+
+#include "cow_se.h"
+
+namespace system_catalog_types {
+    static const gaia_se::gaia_type_t kEventLogType = 6;
+};
