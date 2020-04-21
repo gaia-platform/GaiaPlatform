658d7777245c3ad036d8b85fae049cff9601870a
Dax Hawkins <dax@gaiaplatform.io>
2020-04-21 15:26:17 -0700

    Fixup camera demo to work with new event manager APIs

--- scratch/gregory/cameraDemo/ruleInit.cpp
+++ scratch/gregory/cameraDemo/ruleInit.cpp
@@ -1,26 +1,28 @@
 #include "rules.hpp"
+#include "cameraDemo_gaia_generated.h"
+
 using namespace gaia::rules;
+
 namespace cameraDemo{
-void ObjectClassify_handler(const context_base_t *context);
-}
-namespace cameraDemo{
-void ImageCreate_handler(const context_base_t *context);
+void ObjectClassify_handler(const rule_context_t* context);
 }
 namespace cameraDemo{
-void ImageDelete_handler(const context_base_t *context);
+void ImageCreate_handler(const rule_context_t* context);
 }
-namespace CameraDemo{
-gaia::common::gaia_type_t kObjectType = 2;
+namespace cameraDemo{
+void ImageDelete_handler(const rule_context_t* context);
 }
-namespace CameraDemo{
-gaia::common::gaia_type_t kCameraImageType = 1;
-}
+
 extern "C" void initialize_rules()
 {
-    rule_binding_t  ObjectClassify_handler("cameraDemo", "object_class", cameraDemo::ObjectClassify_handler);
-    rule_binding_t  ImageDelete_handler("cameraDemo", "image_delete", cameraDemo::ImageDelete_handler);
-    rule_binding_t  ImageCreate_handler("cameraDemo", "image_create", cameraDemo::ImageCreate_handler);
-    subscribe_table_rule(CameraDemo::kCameraImageType = 1,  event_type_t::row_insert, ImageCreate_handler);
-    subscribe_table_rule(CameraDemo::kCameraImageType = 1,  event_type_t::row_delete, ImageDelete_handler);
-    subscribe_table_rule(CameraDemo::kObjectType = 2,  event_type_t::row_insert, ObjectClassify_handler);
+    rule_binding_t  ObjectClassify_handler("cameraDemo","rule-object_class",cameraDemo::ObjectClassify_handler);
+    rule_binding_t  ImageDelete_handler("cameraDemo","rule-image_delete",cameraDemo::ImageDelete_handler);
+    rule_binding_t  ImageCreate_handler("cameraDemo","rule-image_create",cameraDemo::ImageCreate_handler);
+
+    subscribe_database_rule(CameraDemo::Camera_image::s_gaia_type, event_type_t::row_insert, ImageCreate_handler);
+
+    subscribe_database_rule(CameraDemo::Camera_image::s_gaia_type, event_type_t::row_delete, ImageDelete_handler);
+
+    subscribe_database_rule(CameraDemo::Object::s_gaia_type, event_type_t::row_insert, ObjectClassify_handler);
+
 }
