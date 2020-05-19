8a5999508489e7edddcb64abf82004a5c199dbef
Tobin Baker <tobin.d.baker@gmail.com>
2020-05-19 12:26:35 -0700

    Fix Postgres FDW test regression from auto-format mangling SQL script.

--- production/sql/src/gaia_fdw.sql
+++ production/sql/src/gaia_fdw.sql
@@ -4,8 +4,8 @@
 ---------------------------------------------
 -- complain if script is sourced in psql, rather than via CREATE EXTENSION
 
-\echo Use "CREATE EXTENSION gaia_fdw" to load this file.
-quit CREATE FUNCTION gaia_fdw_handler ()
+\echo Use "CREATE EXTENSION gaia_fdw" to load this file. \quit
+CREATE FUNCTION gaia_fdw_handler ()
   RETURNS fdw_handler
   AS 'MODULE_PATHNAME'
   LANGUAGE C
