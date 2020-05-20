fe789a79c86b3542a3beb23175c7d9ef89884718
Chuan Liu <liuchuan@liuchuan.org>
2020-05-19 23:28:44 -0700

    init catalog commit

---
+++ production/catalog/catalog.cpp
@@ -0,0 +1,8 @@
+#include "catalog.hpp"
+#include <cstdio>
+#include <cstdlib>
+
+using namespace gaia::catalog;
+void catalog_manager_t::create_database() {}
+void catalog_manager_t::create_table() {}
+void catalog_manager_t::create_field() {}
