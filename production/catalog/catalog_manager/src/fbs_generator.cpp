b79f6caed9b46d06fa4165a93e6e7718f211f032
Chuan Liu <liuchuan@liuchuan.org>
2020-06-16 13:19:32 -0700

    Update fbs generator to remove type notion

--- production/catalog/catalog_manager/src/fbs_generator.cpp
+++ production/catalog/catalog_manager/src/fbs_generator.cpp
@@ -11,50 +11,70 @@
 namespace gaia {
 namespace catalog {
 
-typedef unordered_map<gaia_id_t, std::string> type_id_map_t;
+typedef unordered_map<gaia_id_t, std::string> table_id_map_t;
 typedef std::pair<int, std::string> fieldstr_pair_t;
-typedef std::unordered_map<std::string, vector<fieldstr_pair_t>> type_collection_t;
+typedef std::unordered_map<std::string, vector<fieldstr_pair_t>> table_collection_t;
 
-static std::string generate_field(unique_ptr<GaiaField> &field, type_id_map_t &type_id_map) {
+static std::string get_data_type_name(GaiaDataType e) {
+    string name{EnumNameGaiaDataType(e)};
+    // Convert the data type enum name string to lowercase case
+    // because FlatBuffers schema is case sensitive
+    // and does not recognize uppercase keywords.
+    std::transform(name.begin(), name.end(), name.begin(), ::tolower);
+    return name;
+}
+
+static std::string generate_field(unique_ptr<GaiaField> &field,
+    const table_id_map_t &table_id_map) {
+
     string field_name{field->name()};
 
+    string field_type;
+    if (field->type() != GaiaDataType_TABLE) {
+        field_type = get_data_type_name(field->type());
+    } else {
+        field_type = table_id_map.at(field->type_id());
+    }
+
     if (field->repeated_count() == 1) {
-        return field_name + ":" + type_id_map[field->type_id()];
+        return field_name + ":" + field_type;
     } else if (field->repeated_count() == 0) {
-        return field_name + ":[" + type_id_map[field->type_id()] + "]";
+        return field_name + ":[" + field_type + "]";
     } else {
         return field_name +
-               ":[" + type_id_map[field->type_id()] +
-               ":" + to_string(field->repeated_count()) + "]";
+               ":[" + field_type + ":" + to_string(field->repeated_count()) + "]";
     }
 }
 
 std::string generate_fbs() {
-    type_id_map_t type_id_map;
-    type_collection_t types;
+    table_id_map_t table_id_map;
+    table_collection_t tables;
 
     gaia::db::begin_transaction();
-    for (unique_ptr<GaiaType> t{GaiaType::get_first()}; t; t.reset(t->get_next())) {
-        string type_name{t->name()};
-        std::transform(type_name.begin(), type_name.end(), type_name.begin(), ::tolower);
-        type_id_map[t->gaia_id()] = type_name;
+    for (unique_ptr<GaiaTable> t{GaiaTable::get_first()}; t; t.reset(t->get_next())) {
+        table_id_map[t->gaia_id()] = t->name();
     }
 
-    for (unique_ptr<GaiaField> f{GaiaField::get_first()}; f; f.reset(f->get_next())) {
-        auto e = std::make_pair(f->position(), generate_field(f, type_id_map));
-        if (types.find(type_id_map[f->owner_id()]) == types.end()) {
-            types[type_id_map[f->owner_id()]] = {e};
+    for (unique_ptr<GaiaField> field{GaiaField::get_first()}; field; field.reset(field->get_next())) {
+        fieldstr_pair_t fieldstr_pair = std::make_pair(
+            field->position(),
+            generate_field(field, table_id_map));
+
+        const std::string &table_name = table_id_map[field->table_id()];
+
+        if (tables.find(table_name) == tables.end()) {
+            tables[table_name] = {fieldstr_pair};
         } else {
-            auto &v = types[type_id_map[f->owner_id()]];
-            auto pos = std::lower_bound(v.begin(), v.end(), e,
+            auto &v = tables[table_name];
+            auto pos = std::lower_bound(v.begin(), v.end(), fieldstr_pair,
                 [](fieldstr_pair_t lhs, fieldstr_pair_t rhs) -> bool { return lhs.first < rhs.first; });
-            types[type_id_map[f->owner_id()]].insert(pos, e);
+            tables[table_name].insert(pos, fieldstr_pair);
         }
     }
     gaia::db::commit_transaction();
 
     std::string fbs;
-    for (auto kv : types) {
+    for (auto kv : tables) {
         fbs += "table " + kv.first + "{\n";
         for (auto field_pair : kv.second) {
             fbs += "\t" + field_pair.second + ";\n";
