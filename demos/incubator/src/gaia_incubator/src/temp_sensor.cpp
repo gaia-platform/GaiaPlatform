f924d1708581588a814f3022b4a469125de9eec5
phillipov <phillipov@outlook.com>
2020-07-01 10:10:02 -0700

    [GAIAPLAT-223] temp sensor node publishes a slowly rising temperature

--- demos/incubator/src/gaia_incubator/src/temp_sensor.cpp
+++ demos/incubator/src/gaia_incubator/src/temp_sensor.cpp
@@ -1,11 +1,10 @@
 #include <chrono>
 #include <functional>
 #include <memory>
-#include <string>
 #include <iostream>
 
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
+#include "sensor_msgs/msg/temperature.hpp"
 
 using namespace std;
 using namespace rclcpp;
@@ -17,21 +16,38 @@ public:
     {
         using namespace chrono_literals;
 
-        pubTemp = this->create_publisher<std_msgs::msg::String>("temp", 10);
-        timer = this->create_wall_timer(
-            1s, bind(&TempSensor::pubTemp_callback, this));
+        pub_temp =
+            this->create_publisher<sensor_msgs::msg::Temperature>("temp", 10);
+
+        reading_timer = this->create_wall_timer(
+            1s, bind(&TempSensor::publish_temp, this));
+        loop_timer = this->create_wall_timer(
+            100ms, bind(&TempSensor::calc_new_temp, this));
     }
 
 private:
-    void pubTemp_callback()
-    {
-        auto tempMsg = std_msgs::msg::String();
-        tempMsg.data = "50";
-        pubTemp->publish(tempMsg);
+    void calc_new_temp() {
+        if (fan_speed < 1000)
+            curr_temp += 0.01;
+        else if (fan_speed < 3000)
+            curr_temp -= 0.01;
+        else
+            curr_temp -= 0.02;
     }
 
-    Publisher<std_msgs::msg::String>::SharedPtr pubTemp;
-    TimerBase::SharedPtr timer;
+    void publish_temp()
+    {
+        auto temp_msg = sensor_msgs::msg::Temperature();
+        temp_msg.temperature = curr_temp;
+        pub_temp->publish(temp_msg);
+    }
+
+    Publisher<sensor_msgs::msg::Temperature>::SharedPtr pub_temp;
+    TimerBase::SharedPtr reading_timer;
+    TimerBase::SharedPtr loop_timer;
+
+    double curr_temp = 85.0;
+    double fan_speed = 0.0;
 };
 
 int main(int argc, char* argv[])
