7774807ac21e19ccdbfe12612ebb0a2940245291
Yi Wen Wong <yiwen@gaiaplatform.io>
2020-07-06 11:26:57 -0700

    Create initial directory and makefile for triggers

---
+++ production/db/inc/triggers/events.hpp
@@ -0,0 +1,14 @@
+/////////////////////////////////////////////
+// Copyright (c) Gaia Platform LLC
+// All rights reserved.
+/////////////////////////////////////////////
+
+#pragma once
+
+namespace gaia {
+namespace db {
+namespace events {
+    // TODO: header for events here
+}
+}
+}
