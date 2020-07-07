0a81ab21205b28f50ed66fb3791d8a82ccb1940a
Laurentiu Cristofor <laurentiu@gaiaplatform.io>
2020-07-07 14:30:18 -0700

    Add unit test placeholders

---
+++ production/common/tests/test_queue.cpp
@@ -0,0 +1,17 @@
+/////////////////////////////////////////////
+// Copyright (c) Gaia Platform LLC
+// All rights reserved.
+/////////////////////////////////////////////
+
+#include <iostream>
+
+#include "gtest/gtest.h"
+
+#include <queue.hpp>
+
+using namespace std;
+using namespace gaia::common;
+
+TEST(common, queue)
+{
+}
---
+++ production/db/storage_engine/tests/test_record_list.cpp
@@ -0,0 +1,17 @@
+/////////////////////////////////////////////
+// Copyright (c) Gaia Platform LLC
+// All rights reserved.
+/////////////////////////////////////////////
+
+#include <iostream>
+
+#include "gtest/gtest.h"
+
+#include <record_list.hpp>
+
+using namespace std;
+using namespace gaia::db::storage;
+
+TEST(storage, record_list)
+{
+}
