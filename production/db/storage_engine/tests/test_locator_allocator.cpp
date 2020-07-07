98f32f254f46d50a061238942f6bffd33aaea48d
Laurentiu Cristofor <laurentiu@gaiaplatform.io>
2020-07-07 16:03:14 -0700

    Add locator_allocator_t tests

--- production/db/storage_engine/tests/test_locator_allocator.cpp
+++ production/db/storage_engine/tests/test_locator_allocator.cpp
@@ -14,4 +14,32 @@ using namespace gaia::db::storage;
 
 TEST(storage, locator_allocator)
 {
+    uint64_t locator = locator_allocator_t::get()->allocate_locator();
+    ASSERT_EQ(1, locator);
+
+    locator = locator_allocator_t::get()->allocate_locator();
+    ASSERT_EQ(2, locator);
+
+    locator = locator_allocator_t::get()->allocate_locator();
+    ASSERT_EQ(3, locator);
+
+    locator_allocator_t::get()->release_locator(2);
+    locator_allocator_t::get()->release_locator(1);
+
+    locator = locator_allocator_t::get()->allocate_locator();
+    ASSERT_EQ(2, locator);
+
+    locator = locator_allocator_t::get()->allocate_locator();
+    ASSERT_EQ(1, locator);
+
+    locator = locator_allocator_t::get()->allocate_locator();
+    ASSERT_EQ(4, locator);
+
+    locator_allocator_t::get()->release_locator(3);
+
+    locator = locator_allocator_t::get()->allocate_locator();
+    ASSERT_EQ(3, locator);
+
+    locator = locator_allocator_t::get()->allocate_locator();
+    ASSERT_EQ(5, locator);
 }
