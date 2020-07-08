b7de54460d768caaa20638b41b14fd7f5b2493b7
phillipov <phillipov@outlook.com>
2020-07-07 19:12:20 -0700

    Checking if timestamp crash problem is actually related to RMW selection

--- demos/incubator/src/gaia_incubator/src/incubator_manager.cpp
+++ demos/incubator/src/gaia_incubator/src/incubator_manager.cpp
@@ -100,7 +100,7 @@ void incubator_manager::publish_temp()
             temp_msg.sensor_name = sensor_name;
             // Every sensor in an incubator reports the same temperature.
             temp_msg.value = current_incubator.temperature;
-            temp_msg.header.stamp = now();
+            temp_msg.stamp = now();
 
             m_pub_temp->publish(temp_msg);
         }
