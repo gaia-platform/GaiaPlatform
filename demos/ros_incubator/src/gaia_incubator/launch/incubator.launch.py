f15c2aa45231cb3380fc15e948d081e2763b58ce
phillipov <phillipov@outlook.com>
2020-07-13 16:01:38 -0700

    Launch file now works with composed nodes

--- demos/ros_incubator/src/gaia_incubator/launch/incubator.launch.py
+++ demos/ros_incubator/src/gaia_incubator/launch/incubator.launch.py
@@ -4,21 +4,20 @@ from launch_ros.descriptions import ComposableNode
 
 def generate_launch_description():
     container = ComposableNodeContainer(
-            name='demo_container',
-            namespace='',
-            package='gaia_incubator',
-            executable='incubator_container',
-            composable_node_descriptions=[
-                ComposableNode(
-                    package='gaia_incubator',
-                    plugin='incubator_manager',
-                    name='incubator_manager'),
-                ComposableNode(
-                    package='gaia_incubator',
-                    plugin='gaia_logic',
-                    name='gaia_logic')
-            ],
-            output='screen',
-    )
+        node_name='incubator_container',
+        package='rclcpp_components',
+        node_executable='incubator_container')
+    loader = LoadComposableNodes(
+        composable_node_descriptions=[
+            ComposableNode(
+                package='gaia_incubator',
+                plugin='incubator_manager',
+                name='incubator_manager'),
+            ComposableNode(
+                package='gaia_incubator',
+                plugin='gaia_logic',
+                name='gaia_logic')
+        ],
+        target_container=container, output='screen')
 
-    return launch.LaunchDescription([container])
+    return LaunchDescription([container, loader])
