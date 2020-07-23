08e5150bb0857b1835c0849460fd36af1fdc2bfc
Chuan Liu <liuchuan@liuchuan.org>
2020-07-23 16:48:19 -0700

    Fix test varaible cases

--- production/catalog/parser/tests/test_ddl_parser.cpp
+++ production/catalog/parser/tests/test_ddl_parser.cpp
@@ -30,19 +30,19 @@ TEST(catalog_ddl_parser_test, create_table_multiple_fields) {
     EXPECT_EQ(1, parser.statements.size());
     EXPECT_EQ(parser.statements[0]->type(), statement_type_t::create);
 
-    auto createStmt = dynamic_cast<create_statement_t *>(parser.statements[0].get());
+    auto create_stmt = dynamic_cast<create_statement_t *>(parser.statements[0].get());
 
-    EXPECT_EQ(createStmt->type, create_type_t::create_table);
-    EXPECT_EQ(createStmt->name, "t");
-    EXPECT_EQ(createStmt->fields.size(), 2);
+    EXPECT_EQ(create_stmt->type, create_type_t::create_table);
+    EXPECT_EQ(create_stmt->name, "t");
+    EXPECT_EQ(create_stmt->fields.size(), 2);
 
-    EXPECT_EQ(createStmt->fields.at(0)->name, "c1");
-    EXPECT_EQ(createStmt->fields.at(0)->type, data_type_t::e_int32);
-    EXPECT_EQ(createStmt->fields.at(0)->length, 0);
+    EXPECT_EQ(create_stmt->fields.at(0)->name, "c1");
+    EXPECT_EQ(create_stmt->fields.at(0)->type, data_type_t::e_int32);
+    EXPECT_EQ(create_stmt->fields.at(0)->length, 0);
 
-    EXPECT_EQ(createStmt->fields.at(1)->name, "c2");
-    EXPECT_EQ(createStmt->fields.at(1)->type, data_type_t::e_float64);
-    EXPECT_EQ(createStmt->fields.at(1)->length, 2);
+    EXPECT_EQ(create_stmt->fields.at(1)->name, "c2");
+    EXPECT_EQ(create_stmt->fields.at(1)->type, data_type_t::e_float64);
+    EXPECT_EQ(create_stmt->fields.at(1)->length, 2);
 }
 
 TEST(catalog_ddl_parser_test, create_table_references) {
@@ -52,23 +52,23 @@ TEST(catalog_ddl_parser_test, create_table_references) {
     EXPECT_EQ(1, parser.statements.size());
     EXPECT_EQ(parser.statements[0]->type(), statement_type_t::create);
 
-    auto createStmt = dynamic_cast<create_statement_t *>(parser.statements[0].get());
+    auto create_stmt = dynamic_cast<create_statement_t *>(parser.statements[0].get());
 
-    EXPECT_EQ(createStmt->type, create_type_t::create_table);
-    EXPECT_EQ(createStmt->name, "t");
-    EXPECT_EQ(createStmt->fields.size(), 3);
+    EXPECT_EQ(create_stmt->type, create_type_t::create_table);
+    EXPECT_EQ(create_stmt->name, "t");
+    EXPECT_EQ(create_stmt->fields.size(), 3);
 
-    EXPECT_EQ(createStmt->fields.at(0)->name, "c1");
-    EXPECT_EQ(createStmt->fields.at(0)->type, data_type_t::e_references);
-    EXPECT_EQ(createStmt->fields.at(0)->length, 1);
+    EXPECT_EQ(create_stmt->fields.at(0)->name, "c1");
+    EXPECT_EQ(create_stmt->fields.at(0)->type, data_type_t::e_references);
+    EXPECT_EQ(create_stmt->fields.at(0)->length, 1);
 
-    EXPECT_EQ(createStmt->fields.at(1)->name, "c2");
-    EXPECT_EQ(createStmt->fields.at(1)->type, data_type_t::e_references);
-    EXPECT_EQ(createStmt->fields.at(1)->length, 3);
+    EXPECT_EQ(create_stmt->fields.at(1)->name, "c2");
+    EXPECT_EQ(create_stmt->fields.at(1)->type, data_type_t::e_references);
+    EXPECT_EQ(create_stmt->fields.at(1)->length, 3);
 
-    EXPECT_EQ(createStmt->fields.at(2)->name, "c3");
-    EXPECT_EQ(createStmt->fields.at(2)->type, data_type_t::e_references);
-    EXPECT_EQ(createStmt->fields.at(2)->length, 0);
+    EXPECT_EQ(create_stmt->fields.at(2)->name, "c3");
+    EXPECT_EQ(create_stmt->fields.at(2)->type, data_type_t::e_references);
+    EXPECT_EQ(create_stmt->fields.at(2)->length, 0);
 }
 
 TEST(catalog_ddl_parser_test, drop_table) {
