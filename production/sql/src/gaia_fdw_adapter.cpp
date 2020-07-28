8eb4e9327d1edcde59248d4ed983d23cf496109b
Laurentiu Cristofor <laurentiu.cristofor@outlook.com>
2020-07-28 10:01:56 -0700

    Small refactoring of FDW code to better separate code that needs C linkage from the code that doesn't (#151)
    * Introduce a C++ interface between the FDW and the database
    
    * Further cleanup of extern C declaration scope
    
    * More cleanup of header files
    
    Co-authored-by: Laurentiu Cristofor <laurentiu@gaiaplatform.io>

---
+++ production/sql/src/gaia_fdw_adapter.cpp
@@ -0,0 +1,12 @@
+/////////////////////////////////////////////
+// Copyright (c) Gaia Platform LLC
+// All rights reserved.
+/////////////////////////////////////////////
+
+#include <gaia_fdw_adapter.hpp>
+
+using namespace gaia::fdw;
+
+gaia_fdw_adapter::gaia_fdw_adapter()
+{
+}
