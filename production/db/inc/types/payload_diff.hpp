b9dbb5648407c45d23ba6d51e8e7fb0986e69be0
yiwen-wong <67293355+yiwen-wong@users.noreply.github.com>
2020-08-03 15:36:09 -0700

    Add API to compute data diff (#155)
    This patch introduces the compute_payload_diff() method.
    
    Co-authored-by: Yi Wen Wong <yiwen@gaiaplatform.io>

---
+++ production/db/inc/types/payload_diff.hpp
@@ -0,0 +1,19 @@
+/////////////////////////////////////////////
+// Copyright (c) Gaia Platform LLC
+// All rights reserved.
+/////////////////////////////////////////////
+
+#include "field_list.hpp"
+
+namespace gaia
+{
+namespace db
+{
+namespace types
+{
+
+field_list_t compute_payload_diff(gaia_id_t type_id, const uint8_t* payload1, const uint8_t* payload2);
+
+}
+}
+}
