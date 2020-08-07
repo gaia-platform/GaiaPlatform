b6e65644dc386d7c0f550d3063c0b6912e5283cc
Chuan Liu <liuchuan@liuchuan.org>
2020-08-07 11:32:57 -0700

    [GAIAPLT-262] flatc->fbs APIs and [GAIAPLT-273] constant IDs for catalog tables (#168)
    This change includes two JIRA items:
    
    GAIAPLAT-273: assigns catalog tables constant IDs. As part of the work, the bootstrap process is simplified to remove the DDL definition.
    
    GAIAPLAT-262: switch to use FlatBuffers API instead of calling flatc command. As part of the change, I removed code_writer.cpp to switch to use FlatBuffers version directly.

--- production/db/types/src/payload_diff.cpp
+++ production/db/types/src/payload_diff.cpp
@@ -12,7 +12,7 @@
 #include "data_holder.hpp"
 #include "field_access.hpp"
 #include "gaia_catalog.hpp"
-#include "catalog_gaia_generated.h"
+#include "gaia_catalog.h"
 #include "types.hpp"
 
 namespace gaia
@@ -30,9 +30,9 @@ field_list_t compute_payload_diff(gaia_id_t type_id, const uint8_t* payload1, co
     auto_transaction_t tx;
 
     for (field_position_t i = 0; i < num_fields; i++) {
-        gaia::catalog::Gaia_table table = gaia::catalog::Gaia_table::get(type_id);
+        gaia::catalog::gaia_table_t table = gaia::catalog::gaia_table_t::get(type_id);
         string schema = gaia::catalog::get_bfbs(type_id);
-        field_position_t pos = gaia::catalog::Gaia_field::get(fields[i]).position();
+        field_position_t pos = gaia::catalog::gaia_field_t::get(fields[i]).position();
 
         data_holder_t data_holder1 = get_table_field_value(type_id, payload1, reinterpret_cast<const uint8_t*>(schema.c_str()), pos);
         data_holder_t data_holder2 = get_table_field_value(type_id, payload2, reinterpret_cast<const uint8_t*>(schema.c_str()), pos);
