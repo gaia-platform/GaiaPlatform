9bcd89cb3a0854c4dc375b0e0fa38ef0b033d742
Laurentiu Cristofor <laurentiu@gaiaplatform.io>
2020-08-07 12:26:50 -0700

    Add and fix comments to field_access API

--- production/db/inc/types/field_access.hpp
+++ production/db/inc/types/field_access.hpp
@@ -58,6 +58,16 @@ public:
     invalid_serialized_field_data(field_position_t position);
 };
 
+// GENERAL API NOTES
+//
+// These functions provide read/write access to table record field values.
+//
+// The caller is responsible for handling concurrency issues
+// and for protecting the memory passed in to the API
+// during its execution.
+//
+// The access of this API to the type_cache is protected by the type_cache API.
+
 // Parse the binary schema and insert its Field definitions
 // into the provided field_cache.
 //
@@ -69,7 +79,7 @@ void initialize_field_cache_from_binary_schema(
     const uint8_t* binary_schema);
 
 // Get the field value of a table record payload.
-// The value will be packed in a type_holder_t structure.
+// The value will be packed in a data_holder_t structure.
 //
 // The binary_schema passed in will only be used if the type_cache
 // does not already contain a field_cache entry for the type_id.
@@ -82,6 +92,16 @@ data_holder_t get_field_value(
     const uint8_t* binary_schema,
     field_position_t field_position);
 
+// Set the field value of a table record payload.
+// The new value is provided in a data_holder_t structure.
+//
+// The binary_schema passed in will only be used if the type_cache
+// does not already contain a field_cache entry for the type_id.
+//
+// If the binary_schema is needed but not provided,
+// an invalid_schema() exception will be thrown.
+//
+// This function only works for scalar fields (integers and floating point numbers).
 bool set_field_value(
     gaia_id_t type_id,
     const uint8_t* serialized_data,
