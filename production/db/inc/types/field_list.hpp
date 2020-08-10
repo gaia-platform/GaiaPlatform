245d495a50ad789615b9732ec307dc4ba96f5630
yiwen-wong <67293355+yiwen-wong@users.noreply.github.com>
2020-08-10 10:27:23 -0700

    Remove outdated catalog APIs for field_list_t (#171)
    * Initial checkin
    
    * Rewrite field_list to use direct_access
    
    * use new bootstrap catalog
    
    * Addressed comments
    
    Co-authored-by: Yi Wen Wong <yiwen@gaiaplatform.io>

--- production/db/inc/types/field_list.hpp
+++ production/db/inc/types/field_list.hpp
@@ -11,6 +11,7 @@
 
 #include "gaia_common.hpp"
 #include "gaia_exception.hpp"
+#include "types.hpp"
 
 using namespace std;
 using namespace gaia::common;
@@ -35,16 +36,16 @@ class field_list_t {
     private:
     static const size_t c_max_vector_reserve = 8;
 
-    gaia_id_t m_table_id;
+    gaia_id_t m_type_id;
 
     // Backing structure for this list.
-    unique_ptr< vector<gaia_id_t> > m_data;
+    unique_ptr< vector<field_position_t> > m_data;
     // Initialize the backing structure.
     void initialize();
 
     public:
-    // Constructor, accepts table_id for this list as the input.
-    field_list_t(gaia_id_t table_id);
+    // Constructor, accepts type_id for this list as the input.
+    field_list_t(gaia_id_t type_id);
 
     // Copy and move.
     field_list_t(const field_list_t& other);
@@ -52,10 +53,10 @@ class field_list_t {
     field_list_t(field_list_t&& other) = default;
     field_list_t& operator=(field_list_t&& other) = default;
 
-    gaia_id_t operator[](size_t idx) const;
+    field_position_t operator[](size_t idx) const;
 
     // Is this field a member?
-    bool contains(gaia_id_t field_id) const;
+    bool contains(field_position_t field_pos) const;
 
     // Number of fields in this list.
     size_t size() const;
@@ -64,7 +65,7 @@ class field_list_t {
     bool is_empty() const;
 
     // Add individual fields.
-    void add(gaia_id_t field_id);
+    void add(field_position_t field_pos);
 
     // Additional binary operations.
     field_list_t intersect(const field_list_t& other) const;
@@ -72,8 +73,8 @@ class field_list_t {
     // Validate: check if this list is valid against the catalog.
     bool validate() const;
 
-    // Getter: table_id.
-    gaia_id_t get_table_id() const;
+    // Getter: type_id.
+    gaia_id_t get_type_id() const;
 };
 
 }
