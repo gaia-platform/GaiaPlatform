7b9dbda0928fb9ce0a1bc7e92401fd93c9ad3505
Cameron Evans <cameron@gaiaplatform.io>
2020-09-01 12:17:01 -0700

    Fix gdev multiple mixins fails to build

--- dev_tools/gdev/gdev/cmd/gen/_custom/build.py
+++ dev_tools/gdev/gdev/cmd/gen/_custom/build.py
@@ -15,7 +15,7 @@ class GenCustomBuild(GenAbcBuild):
     async def _get_wanted_label_value_by_name(self) -> Mapping[str, str]:
         return {
             **await super()._get_wanted_label_value_by_name(),
-            'Mixins': f'{sorted(self.options.mixins)}'
+            'Mixins': f'{sorted(self.options.mixins)}'.replace(' ', '')
         }
 
     @memoize
