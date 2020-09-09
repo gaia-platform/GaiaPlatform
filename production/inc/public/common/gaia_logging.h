daf2f0842f170ced2c64871a54c98f1e1678e880
Simone Rondelli <simone@gaiaplatform.com>
2020-09-09 15:57:31 -0400

    Start logging implementation

---
+++ production/inc/public/common/gaia_logging.h
@@ -0,0 +1,20 @@
+/////////////////////////////////////////////
+// Copyright (c) Gaia Platform LLC
+// All rights reserved.
+/////////////////////////////////////////////
+
+#pragma once
+
+namespace gaia::common::logging {
+
+enum class gaia_log_level_t {
+
+};
+
+class gaia_log_t {
+  public:
+    void log();
+
+
+};
+} // namespace gaia::common::logging
