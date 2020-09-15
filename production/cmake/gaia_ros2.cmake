429c1d43a7fedae70f5ad73bd3a588f5dfc893e2
phillipov <phillipov@outlook.com>
2020-09-14 18:32:23 -0700

    Ported recursive message discovery to cmake

--- production/cmake/gaia_ros2.cmake
+++ production/cmake/gaia_ros2.cmake
@@ -8,20 +8,22 @@ set(built_in_types "bool;byte;char;float32;float64;int8;uint8;int16;uint16;int32
 #
 # Converts ROS2 .msg files into DDL schemas for Gaia table creation.
 #
-function(ros2_msg_to_gaia_ddl ros2_pkg interface_files dependencies)
+function(ros2_msg_to_gaia_ddl)
   set(interface_list "")
-  find_package(${ros2_pkg})
 
-  foreach(interface_file ${interface_files})
-    list(APPEND interface_list "${ros2_pkg} ${interface_file}")
+  foreach(interface_arg ${ARGN})
+    list(APPEND interface_list "${interface_arg}")
 
     set(nested_msgs "")
-    get_nested_msgs(${ros2_pkg} ${interface_file} nested_msgs)
+    get_nested_msgs(${interface_arg} nested_msgs)
 
     foreach(nested_msg ${nested_msgs})
-      list(APPEND interface_list "NESTED: ${nested_msg}")
+      list(APPEND interface_list "${nested_msg}")
     endforeach()
   endforeach()
+
+  list(REMOVE_DUPLICATES interface_list)
+  list(SORT interface_list)
 
   print_list("${interface_list}")
 endfunction(ros2_msg_to_gaia_ddl)
@@ -29,22 +31,63 @@ endfunction(ros2_msg_to_gaia_ddl)
 #
 # Reads a ROS2 interface file to find field types that are ROS2 messages.
 #
-function(get_nested_msgs ros2_pkg interface_file nested_msgs_result)
+function(get_nested_msgs interface_arg nested_msgs_result)
+  set(ros2_pkg "")
+  string(REGEX MATCH "^[a-z0-9_]*" ros2_pkg ${interface_arg})
+  set(interface_file "")
+  string(REGEX MATCH "[A-Za-z0-9_\\/\\.]*$" interface_file ${interface_arg})
+
   find_package(${ros2_pkg})
   set(interface_file_path "${${ros2_pkg}_DIR}/../${interface_file}")
-  set(nested_msgs "")
+
   set(file_lines "")
+  # Remove comments (they begin with #) and reject leading whitespace.
+  file(STRINGS ${interface_file_path} file_lines REGEX "^[^# \t\r\n]")
 
-  file(STRINGS ${interface_file_path} file_lines REGEX "^[^#]")
-
+  set(nested_msgs "")
   foreach(file_line ${file_lines})
-    string(REGEX MATCH "^[A-Za-z0-9_\\/]*" file_line ${file_line})
+    set(field_type "")
+    # Extract the field type, which is the first word before the space,
+    # excluding the square brackets from non-scalar types.
+    string(REGEX MATCH "^[A-Za-z0-9_\\/]*" field_type ${file_line})
 
-    if(NOT "${file_line}" IN_LIST built_in_types)
-      list(APPEND nested_msgs "${file_line}")
+    # Ignore built-in types because they are not nested messages.
+    if("${field_type}" IN_LIST built_in_types)
+      continue()
+    endif()
+
+    # If a nested message is from a different package, its field type will
+    # contain a forward-slash. Otherwise, it comes from the same package
+    # as its parent message.
+    # Format the field type as "<package> msg/<message>.msg".
+    if(field_type MATCHES "^.*\\/.*$")
+      string(REPLACE "/" " msg/" field_type ${field_type})
+    else()
+      string(PREPEND field_type "${ros2_pkg} msg/")
+    endif()
+    string(APPEND field_type ".msg")
+
+    list(APPEND nested_msgs "${field_type}")
+  endforeach()
+
+  list(REMOVE_DUPLICATES nested_msgs)
+
+  set(deeper_msgs "")
+  foreach(nested_msg ${nested_msgs})
+    set(nested_pkg "")
+    string(REGEX MATCH "^[a-z0-9_]*" nested_pkg ${nested_msg})
+    set(nested_interface_file "")
+    string(REGEX MATCH "[A-Za-z0-9_\\/\\.]*$" nested_interface_file ${nested_msg})
+    set(deeper_result "")
+
+    get_nested_msgs("${nested_pkg} ${nested_interface_file}" deeper_result)
+    if(NOT "${deeper_result}" STREQUAL "")
+      list(APPEND deeper_msgs "${deeper_result}")
     endif()
   endforeach()
 
+  list(APPEND nested_msgs "${deeper_msgs}")
+  list(REMOVE_DUPLICATES nested_msgs)
   set(${nested_msgs_result} "${nested_msgs}" PARENT_SCOPE)
 endfunction(get_nested_msgs)
 
@@ -56,12 +99,3 @@ function(print_list list_to_print)
     message(NOTICE ${element})
   endforeach()
 endfunction(print_list)
-
-#foreach(dependency ${DEPENDENCIES})
-#  find_package(${dependency})
-#  message(NOTICE ">> dep: ${dependency}")
-#  message(NOTICE "recursive dependencies: ${${dependency}_RECURSIVE_DEPENDENCIES}")
-#  foreach(dependency_interface_file ${${dependency}_INTERFACE_FILES})
-#    message(NOTICE "  -- ${${dependency}_DIR}/../${dependency_interface_file}")
-#  endforeach()
-#endforeach()
