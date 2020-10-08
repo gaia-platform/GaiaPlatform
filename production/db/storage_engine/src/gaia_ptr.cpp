2b163062ab94544b32ea49c7cca66782273da16c
Chuan Liu <liuchuan@liuchuan.org>
2020-10-08 11:21:13 -0700

    Add an update child ref method to for gaia_iterators (#243)
    A minor update to gaia_iterator: update the ref update method name to reflect its true intention.

--- production/db/storage_engine/src/gaia_ptr.cpp
+++ production/db/storage_engine/src/gaia_ptr.cpp
@@ -80,16 +80,6 @@ gaia_ptr& gaia_ptr::update_payload(size_t data_size, const void* data) {
     return *this;
 }
 
-gaia_ptr& gaia_ptr::update_parent_references(size_t child_slot, gaia_id_t child_id) {
-    gaia_offset_t old_offset = to_offset();
-    clone_no_tx();
-
-    references()[child_slot] = child_id;
-
-    client::tx_log(m_locator, old_offset, to_offset(), gaia_operation_t::update);
-    return *this;
-}
-
 gaia_ptr& gaia_ptr::update_child_references(
     size_t next_child_slot, gaia_id_t next_child_id,
     size_t parent_slot, gaia_id_t parent_id) {
@@ -98,6 +88,16 @@ gaia_ptr& gaia_ptr::update_child_references(
 
     references()[next_child_slot] = next_child_id;
     references()[parent_slot] = parent_id;
+
+    client::tx_log(m_locator, old_offset, to_offset(), gaia_operation_t::update);
+    return *this;
+}
+
+gaia_ptr& gaia_ptr::update_child_reference(size_t child_slot, gaia_id_t child_id) {
+    auto old_offset = to_offset();
+    clone_no_tx();
+
+    references()[child_slot] = child_id;
 
     client::tx_log(m_locator, old_offset, to_offset(), gaia_operation_t::update);
     return *this;
