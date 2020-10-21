8441a245682c390156bcb5ebcdd45ebc7c689d47
Laurentiu Cristofor <laurentiu@gaiaplatform.io>
2020-10-21 15:17:08 -0700

    Fix test #3

--- production/catalog/catalog_manager/src/catalog_manager.cpp
+++ production/catalog/catalog_manager/src/catalog_manager.cpp
@@ -182,12 +182,7 @@ void catalog_manager_t::bootstrap_catalog()
         fields.emplace_back(
             make_unique<field_definition_t>(c_empty_c_str, data_type_t::e_references, 1, "catalog.gaia_ruleset"));
         create_table_impl(
-            "catalog",
-            "gaia_rule",
-            fields,
-            false,
-            false, // NOLINT
-            static_cast<gaia_id_t>(catalog_table_type_t::gaia_rule));
+            "catalog", "gaia_rule", fields, false, false, static_cast<gaia_id_t>(catalog_table_type_t::gaia_rule));
     }
 }
 
@@ -211,12 +206,7 @@ void catalog_manager_t::create_system_tables()
         fields.emplace_back(make_unique<field_definition_t>("timestamp", data_type_t::e_uint64, 1));
         fields.emplace_back(make_unique<field_definition_t>("rules_invoked", data_type_t::e_bool, 1));
         create_table_impl(
-            "event_log",
-            "event_log",
-            fields,
-            true,
-            false, // NOLINT
-            static_cast<gaia_id_t>(system_table_type_t::event_log));
+            "event_log", "event_log", fields, true, false, static_cast<gaia_id_t>(system_table_type_t::event_log));
     }
 }
 
@@ -381,7 +371,7 @@ static gaia_ptr insert_gaia_table_row(
     return gaia_ptr::create(
         table_id,
         static_cast<gaia_type_t>(catalog_table_type_t::gaia_table),
-        c_gaia_table_num_refs, // NOLINT
+        c_gaia_table_num_refs,
         fbb.GetSize(),
         fbb.GetBufferPointer());
 }
