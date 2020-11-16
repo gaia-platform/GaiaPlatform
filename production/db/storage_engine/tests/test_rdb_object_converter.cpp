8cc11f70f448bb15e6aafa2f10e19f7286fe6f6c
Tobin Baker <tobin.d.baker@gmail.com>
2020-11-16 13:20:52 -0800

    Enable ASan in debug builds and fix ASan violations. (#351)

--- production/db/storage_engine/tests/test_rdb_object_converter.cpp
+++ production/db/storage_engine/tests/test_rdb_object_converter.cpp
@@ -4,20 +4,21 @@
 #include "rdb_object_converter.hpp"
 #include "storage_engine.hpp"
 
+using namespace gaia::db::persistence;
+
 TEST(write_read_assert, basic)
 {
-    gaia::db::string_writer_t writer;
+    string_writer_t writer;
 
     u_int64_t e1 = 3423942943286;
     int64_t e2 = 4673438;
     u_int32_t e3 = 3423942946;
     std::string payload = "hello there 37462382038%%%^#*^@93)!@(*)@7  #@^!#)!@*#&";
     u_int32_t e5 = 467845;
-    u_char e6 = u_char(0);
-    u_char e7 = '*';
+    uint8_t e6 = 0;
+    uint8_t e7 = '*';
     std::string empty_payload = "";
-
-    rocksdb::Slice s;
+    ASSERT_EQ(empty_payload.length(), 0);
 
     // Fill write buffer
     writer.write_uint64(e1);
@@ -33,29 +34,33 @@ TEST(write_read_assert, basic)
     writer.write(empty_payload.c_str(), empty_payload.length());
 
     // Create reader & read in the same order of writes
-    s = writer.to_slice();
-    gaia::db::string_reader_t reader(&s);
+    rocksdb::Slice s = writer.to_slice();
+    string_reader_t reader(s);
 
     u_int64_t v1;
     u_int32_t v2;
-    u_char c;
+    uint8_t c;
 
-    EXPECT_NO_THROW(reader.read_uint64(&v1));
+    EXPECT_NO_THROW(reader.read_uint64(v1));
     EXPECT_EQ(e1, v1);
-    EXPECT_NO_THROW(reader.read_uint64(&v1));
+    EXPECT_NO_THROW(reader.read_uint64(v1));
     EXPECT_EQ(e2, v1);
-    EXPECT_NO_THROW(reader.read_uint32(&v2));
+    EXPECT_NO_THROW(reader.read_uint32(v2));
     EXPECT_EQ(e3, v2);
     const char* c1 = reader.read(payload.length());
+    // We can't use EXPECT_STREQ() here, since the embedded string isn't null-terminated.
+    EXPECT_EQ(0, std::memcmp(payload.data(), c1, payload.length()));
     EXPECT_STREQ(payload.data(), c1);
-    EXPECT_NO_THROW(reader.read_uint32(&v2));
+    EXPECT_NO_THROW(reader.read_uint32(v2));
     EXPECT_EQ(e5, v2);
-    EXPECT_NO_THROW(reader.read_byte(&c));
+    EXPECT_NO_THROW(reader.read_byte(c));
     EXPECT_EQ(e6, c);
-    EXPECT_NO_THROW(reader.read_byte(&c));
+    EXPECT_NO_THROW(reader.read_byte(c));
     EXPECT_EQ(e7, c);
+    // We should get a null pointer from a read after we've exhausted the buffer
+    // (otherwise we'd be reading past the end of the buffer).
     const char* c2 = reader.read(0);
-    EXPECT_STREQ(empty_payload.data(), c2);
+    EXPECT_EQ(nullptr, c2);
     EXPECT_EQ(0, reader.get_remaining_len_in_bytes());
 }
 
@@ -64,22 +69,21 @@ TEST(write_read_assert, payload_edge_case)
 {
     // Our payloads are at the end of the value slice
     // hence we don't need to deal with encoding empty strings.
-    gaia::db::string_writer_t writer;
-    u_char e = '*';
+    string_writer_t writer;
+    uint8_t e = '*';
     std::string empty_payload = "";
 
     writer.write(empty_payload.data(), empty_payload.length());
     writer.write_uint8(e);
 
     rocksdb::Slice s = writer.to_slice();
-    gaia::db::string_reader_t reader(&s);
+    string_reader_t reader(s);
 
     const char* c = reader.read(0);
-    EXPECT_STRNE(empty_payload.data(), c);
+    // We can't use EXPECT_STRNE() here, since the embedded string isn't null-terminated.
+    EXPECT_EQ(0, std::memcmp(empty_payload.data(), c, empty_payload.length()));
 
-    EXPECT_STRNE("*", c);
-
-    u_char c2;
-    reader.read_byte(&c2);
+    uint8_t c2;
+    reader.read_byte(c2);
     EXPECT_EQ(e, c2);
 }
