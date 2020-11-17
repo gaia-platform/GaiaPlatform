c9ebeb5871b36fc070e2176a78f341682f88bf6e
Tobin Baker <tobin.d.baker@gmail.com>
2020-11-16 21:50:49 -0800

    Add a text mode option to file_loader (#357)
    * Add a text mode option to file_loader
    
    * apply CR feedback
    
    * minor cleanup

--- production/inc/internal/common/file.hpp
+++ production/inc/internal/common/file.hpp
@@ -7,7 +7,7 @@
 
 #include <string>
 
-#include <gaia_exception.hpp>
+#include "gaia_exception.hpp"
 
 namespace gaia
 {
@@ -37,17 +37,15 @@ public:
 class file_loader_t
 {
 public:
-
     file_loader_t();
     ~file_loader_t();
 
-    size_t load_file_data(const std::string& filename);
+    size_t load_file_data(const std::string& filename, bool enable_text_mode = false);
 
     uint8_t* get_data();
     size_t get_data_length();
 
 protected:
-
     void initialize();
     void clear();
 
@@ -57,6 +55,6 @@ protected:
 };
 
 /*@}*/
-}
+} // namespace common
 /*@}*/
-}
+} // namespace gaia
