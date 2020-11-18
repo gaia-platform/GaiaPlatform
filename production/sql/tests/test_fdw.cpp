4b9224a1e25da62d1bc226f662e0568de329d875
Simone Rondelli <simone@gaiaplatform.com>
2020-11-18 12:20:12 -0500

    Rename airlines to airport

--- production/sql/tests/test_fdw.cpp
+++ production/sql/tests/test_fdw.cpp
@@ -19,7 +19,7 @@ class fdw_test : public db_catalog_test_base_t
 {
 protected:
     fdw_test()
-        : db_catalog_test_base_t("airlines.ddl"){};
+        : db_catalog_test_base_t("airport.ddl"){};
 
     static field_def_list_t airlines_table_fields;
     static field_def_list_t airports_table_fields;
