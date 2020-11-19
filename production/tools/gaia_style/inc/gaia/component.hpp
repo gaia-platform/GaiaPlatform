5e508c9bb5208fd60961175c1765ef1e86bbc311
Simone Rondelli <simone@gaiaplatform.com>
2020-11-19 10:36:55 -0500

    Add .clang-format rule to sort gaia/ headers

---
+++ production/tools/gaia_style/inc/gaia/component.hpp
@@ -0,0 +1,18 @@
+/////////////////////////////////////////////
+// Copyright (c) Gaia Platform LLC
+// All rights reserved.
+/////////////////////////////////////////////
+
+#pragma once
+
+// fake gaia component to test the header sorting functionality
+
+namespace gaia
+{
+namespace style
+{
+
+constexpr int c_variable_that_gives_a_meaning_to_this_header = 1;
+
+} // namespace style
+} // namespace gaia
