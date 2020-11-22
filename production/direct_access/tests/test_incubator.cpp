f5534a2f61996bf31202d891e58a3567ec92f8fd
Chuan Liu <liuchuan@liuchuan.org>
2020-11-21 20:45:23 -0800

    Add the incubator unit test

---
+++ production/direct_access/tests/test_incubator.cpp
@@ -0,0 +1,62 @@
+/////////////////////////////////////////////
+// Copyright (c) Gaia Platform LLC
+// All rights reserved.
+/////////////////////////////////////////////
+
+#include <string>
+
+#include <gtest/gtest.h>
+
+#include "auto_transaction.hpp"
+#include "db_catalog_test_base.hpp"
+#include "gaia_barn_storage.h"
+#include "gaia_common.hpp"
+
+using namespace gaia::barn_storage;
+
+class gaia_incubator_test : public db_catalog_test_base_t
+{
+protected:
+    gaia_incubator_test()
+        : db_catalog_test_base_t(std::string("barn_storage.ddl")){};
+
+    gaia_id_t insert_incubator(const char* name, float min_temp, float max_temp)
+    {
+        incubator_writer w;
+        w.name = name;
+        w.min_temp = min_temp;
+        w.max_temp = max_temp;
+        return w.insert_row();
+    }
+};
+
+// Make sure incubator demo schema works for read and write.
+TEST_F(gaia_incubator_test, schema_read_write_test)
+{
+    const char c_chicken[] = "Simone";
+    const float c_chicken_min = 76.7f;
+    const float c_chicken_max = 83.2f;
+    const char c_sensor_a[] = "temp";
+    gaia_id_t incubator_id = c_invalid_gaia_id;
+    {
+        auto_transaction_t txn;
+        incubator_id = insert_incubator(c_chicken, c_chicken_min, c_chicken_max);
+        ASSERT_TRUE(incubator_id != c_invalid_gaia_id);
+        auto incubator = incubator_t::get(incubator_id);
+        auto sensor = sensor_t::insert_row(c_sensor_a, 0, c_chicken_min);
+        incubator.sensor_list().insert(sensor);
+        txn.commit();
+    }
+
+    {
+        auto_transaction_t txn;
+        auto incubator = incubator_t::get(incubator_id);
+        ASSERT_STREQ(incubator.name(), c_chicken);
+        ASSERT_FLOAT_EQ(incubator.max_temp(), c_chicken_max);
+        ASSERT_FLOAT_EQ(incubator.min_temp(), c_chicken_min);
+        auto sensor_list = incubator.sensor_list();
+        ASSERT_EQ(1, distance(sensor_list.begin(), sensor_list.end()));
+        ASSERT_STREQ(sensor_list.begin()->name(), c_sensor_a);
+        ASSERT_FLOAT_EQ(sensor_list.begin()->value(), c_chicken_min);
+    }
+}
