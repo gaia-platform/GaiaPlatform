9da417769a050f6e55489698947e196661ce927a
telemething <mark@telemething.com>
2020-12-01 09:37:34 -0800

    Remove vestigal temp demo resources

--- demos/Campus/edge/start_display.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export ADLINK_DATARIVER_URI=file:///media/mark/Data1/U20/SDK/ADLINK/EdgeSDK/1.5.1/etc/config/default_datariver_config_v1.6.xml
-
-THING_PROPERTIES_URI=file://./config/TemperatureDisplayProperties.json
-RUNNING_TIME=${1:-60}
-
-./temperaturedisplay $THING_PROPERTIES_URI $RUNNING_TIME
--- demos/Campus/edge/start_sensor.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export ADLINK_DATARIVER_URI=file:///media/mark/Data1/U20/SDK/ADLINK/EdgeSDK/1.5.1/etc/config/default_datariver_config_v1.6.xml
-
-THING_PROPERTIES_URI=file://./config/TemperatureSensorProperties.json
-RUNNING_TIME=${1:-60}
-
-./temperaturesensor $THING_PROPERTIES_URI $RUNNING_TIME
