771635f0c5a5d5f812d5503976ceefbb481698cf
Laurentiu Cristofor <laurentiu@gaiaplatform.io>
2020-12-10 13:19:37 -0800

    Use clang8 instead of clang10

--- production/examples/hello/build.sh
+++ production/examples/hello/build.sh
@@ -9,4 +9,4 @@ gaiac -g hello.ddl
 
 gaiat hello.ruleset -output hello_rules.cpp -- -I /usr/lib/gcc/x86_64-linux-gnu/9/include/ -I /opt/gaia/include/
 
-clang++-10 hello.cpp hello_rules.cpp /usr/local/lib/libgaia.so -I /opt/gaia/include -Wl,-rpath,/usr/local/lib -o hello
+clang++-8 hello.cpp hello_rules.cpp /usr/local/lib/libgaia.so -I /opt/gaia/include -Wl,-rpath,/usr/local/lib -o hello
