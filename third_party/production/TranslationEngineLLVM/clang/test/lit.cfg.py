066069d28b64aca359b953ecd217d846f97b9360
gregory <gregory@gaiaplatform.io>
2020-12-15 21:55:37 -0800

    Improve test reliability by disabling server persistence

--- third_party/production/TranslationEngineLLVM/clang/test/lit.cfg.py
+++ third_party/production/TranslationEngineLLVM/clang/test/lit.cfg.py
@@ -199,12 +199,12 @@ try:
     import psutil
     lit_config.note('Found python psutil module')
     for p in psutil.process_iter(['name']):
-        if p.info['name'] == 'gaia_se_server':
+        if p.info['name'] == "gaia_se_server":
             p.kill()
-    if os.path.exists(config.gaia_binary_dir + '/db/storage_engine'):
-        psutil.Popen([config.gaia_binary_dir + "/db/storage_engine/gaia_se_server"])
+    if os.path.exists(config.gaia_binary_dir + "/db/storage_engine"):
+        psutil.Popen([config.gaia_binary_dir + "/db/storage_engine/gaia_se_server", "--disable-persistence"])
         time.sleep(1)
-    if os.path.exists(config.gaia_binary_dir + '/catalog/gaiac'):
+    if os.path.exists(config.gaia_binary_dir + "/catalog/gaiac"):
         psutil.Popen([config.gaia_binary_dir + "/catalog/gaiac/gaiac", config.gaia_source_dir + "/third_party/production/TranslationEngineLLVM/clang/test/Parser/barn_storage.ddl"])
 except ImportError:
     lit_config.warning('Could not import psutil. Some tests will be skipped and'
