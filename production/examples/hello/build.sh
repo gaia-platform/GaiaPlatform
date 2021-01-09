02c47eecbfbd62a76470db31cbbc78f7e7246058
Simone Rondelli <simone@gaiaplatform.com>
2021-01-08 20:14:30 -0500

    Address PR comments

--- production/examples/hello/build.sh
+++ production/examples/hello/build.sh
@@ -9,4 +9,4 @@ gaiac -g hello.ddl
 
 gaiat hello.ruleset -output hello_rules.cpp -- -I /usr/lib/clang/8/include/ -I /opt/gaia/include/
 
-clang++-8 hello.cpp hello_rules.cpp /usr/local/lib/libgaia.so -I /opt/gaia/include -Wl,-rpath,/usr/local/lib -o hello -lpthread
+clang++-8 hello.cpp hello_rules.cpp /usr/local/lib/libgaia.so -I /opt/gaia/include -Wl,-rpath,/usr/local/lib -lpthread -o hello
