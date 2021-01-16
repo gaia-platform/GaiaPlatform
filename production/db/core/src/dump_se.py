4979f5ec1807a449410a9c12b8a66a4e3c7a0375
Laurentiu Cristofor <laurentiu.cristofor@outlook.com>
2021-01-15 16:11:33 -0800

    Update pybind db_client wrapper and re-enable its unit test (#492)
    * Update pybind db_client wrapper and re-enable its unit test
    
    * Update Python script for database dump
    
    Co-authored-by: Laurentiu Cristofor <laurentiu@gaiaplatform.io>

--- production/db/core/src/dump_se.py
+++ production/db/core/src/dump_se.py
@@ -3,7 +3,7 @@
 # All rights reserved.
 #############################################
 
-from db_client import *;
+from gaia_db_pybind import *;
 
 begin_session();
 
@@ -13,35 +13,22 @@ type = 1;
 done = False;
 while not(done):
     no_node = False;
-    no_edge = False;
 
     print("Iterating through nodes of type: " + str(type) + "...");
-    node_iter = gaia_se_node_ptr.find_first(type);
+    node_iter = gaia_ptr.find_first(type);
 
-    if (node_iter.get() == None):
+    if (node_iter.is_null() == True):
         print("No nodes of type " + str(type) + " were found!");
         no_node = True;
 
-    while node_iter.get() != None:
+    while node_iter.is_null() != True:
         print_node(node_iter, False);
         node_iter = node_iter.find_next();
     print ("");
 
-    print("Iterating through edges of type " + str(type) + "...");
-    edge_iter = gaia_se_edge_ptr.find_first(type);
-
-    if (edge_iter.get() == None):
-        print("No edges of type " + str(type) + " were found!");
-        no_edge = True;
-
-    while edge_iter.get() != None:
-        print_edge(edge_iter, False);
-        edge_iter = edge_iter.find_next();
-    print ("===");
-
-    if (no_node and no_edge):
-        print("No nodes or edges of type " + str(type) + " were found!");
-        print("Terminating graph scan!");
+    if (no_node):
+        print("No nodes of type " + str(type) + " were found!");
+        print("Terminating database scan!");
         done = True;
 
     type = type + 1;
