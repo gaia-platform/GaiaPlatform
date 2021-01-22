ddeb0bdfa268063aed5ea42ebf5694c5378f7af4
Cameron Evans <cameron@gaiaplatform.io>
2021-01-22 13:43:41 -0800

    Address PR feedback.
    Assert that gaia_db_server does not allow any mutually exclusive flag
    pairs.
    
    Moved creation of /var/lib/gaia/db from gdev.cfg's [run] to [pre_run].

--- production/db/core/src/db_server_exec.cpp
+++ production/db/core/src/db_server_exec.cpp
@@ -3,6 +3,7 @@
 // All rights reserved.
 /////////////////////////////////////////////
 
+#include <initializer_list>
 #include <iostream>
 #include <string>
 
@@ -31,48 +32,58 @@ static void usage()
 
 int main(int argc, char* argv[])
 {
-    int mutually_exclusive_persistence_flags{0};
     server::persistence_mode_t persistence_mode{server::persistence_mode_t::e_default};
 
-    for (int i = 1; i < argc; ++i)
     {
-        if (strcmp(argv[i], gaia::db::server::c_disable_persistence_flag) == 0)
+        std::set<std::string> used_flags;
+        for (int i = 1; i < argc; ++i)
         {
-            persistence_mode = server::persistence_mode_t::e_disabled;
-            mutually_exclusive_persistence_flags++;
+            used_flags.insert(argv[i]);
+            if (strcmp(argv[i], gaia::db::server::c_disable_persistence_flag) == 0)
+            {
+                persistence_mode = server::persistence_mode_t::e_disabled;
+            }
+            else if (strcmp(argv[i], gaia::db::server::c_disable_persistence_after_recovery_flag) == 0)
+            {
+                persistence_mode = server::persistence_mode_t::e_disabled_after_recovery;
+            }
+            else if (strcmp(argv[i], gaia::db::server::c_reinitialize_persistent_store_flag) == 0)
+            {
+                persistence_mode = server::persistence_mode_t::e_reinitialized_on_startup;
+            }
+            else if ((strcmp(argv[i], gaia::db::persistent_store_manager::c_data_dir_command_flag) == 0) && (i + 1 < argc))
+            {
+                gaia::db::persistent_store_manager::s_data_dir_path = argv[++i];
+            }
+            else
+            {
+                std::cerr
+                    << std::endl
+                    << "Unrecognized argument."
+                    << std::endl;
+                usage();
+            }
         }
-        else if (strcmp(argv[i], gaia::db::server::c_disable_persistence_after_recovery_flag) == 0)
+        for (const auto& flag_pair : std::initializer_list<std::pair<std::string, std::string>>{
+                 // Disable persistence flag is mutually exclusive with specifying data directory for persistence.
+                 {gaia::db::server::c_disable_persistence_flag, gaia::db::persistent_store_manager::c_data_dir_command_flag},
+                 // The three persistence flags that are mutually exclusive with each other.
+                 {gaia::db::server::c_disable_persistence_flag, gaia::db::server::c_disable_persistence_after_recovery_flag},
+                 {gaia::db::server::c_disable_persistence_flag, gaia::db::server::c_reinitialize_persistent_store_flag},
+                 {gaia::db::server::c_disable_persistence_after_recovery_flag, gaia::db::server::c_reinitialize_persistent_store_flag}})
         {
-            persistence_mode = server::persistence_mode_t::e_disabled_after_recovery;
+            if ((used_flags.find(flag_pair.first) != used_flags.end()) && (used_flags.find(flag_pair.second) != used_flags.end()))
+            {
+                std::cerr
+                    << std::endl
+                    << flag_pair.first
+                    << " and "
+                    << flag_pair.second
+                    << " flags are mutually exclusive."
+                    << std::endl;
+                usage();
+            }
         }
-        else if (strcmp(argv[i], gaia::db::server::c_reinitialize_persistent_store_flag) == 0)
-        {
-            persistence_mode = server::persistence_mode_t::e_reinitialized_on_startup;
-        }
-        else if ((strcmp(argv[i], gaia::db::persistent_store_manager::c_data_dir_command_flag) == 0) && (i + 1 < argc))
-        {
-            gaia::db::persistent_store_manager::s_data_dir_path = argv[++i];
-            mutually_exclusive_persistence_flags++;
-        }
-        else
-        {
-            std::cerr
-                << std::endl
-                << "Unrecognized argument."
-                << std::endl;
-            usage();
-        }
-    }
-    if (mutually_exclusive_persistence_flags > 1)
-    {
-        std::cerr
-            << std::endl
-            << gaia::db::server::c_disable_persistence_flag
-            << " and "
-            << gaia::db::persistent_store_manager::c_data_dir_command_flag
-            << " flags are mutually exclusive."
-            << std::endl;
-        usage();
     }
 
     gaia::db::server::run(persistence_mode);
