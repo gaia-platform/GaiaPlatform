4fddb8dd178ea6f65cf87db5ecd8ab414bd66389
Simone Rondelli <simone@gaiaplatform.com>
2021-01-28 12:02:00 -0500

    [GAIAPLAT-508] Add ping_pong application to test high throughput applications

---
+++ production/examples/ping_pong/README.md
@@ -0,0 +1,4 @@
+# ping_pong
+
+This is a simple example of high throughput application. The purpose is to test Gaia capabilities with high throughput
+applications.
---
+++ production/examples/ping_pong/ping_pong.cpp
@@ -0,0 +1,43 @@
+/////////////////////////////////////////////
+// Copyright (c) Gaia Platform LLC
+// All rights reserved.
+/////////////////////////////////////////////
+
+#include "gaia/logger.hpp"
+#include "gaia/system.hpp"
+
+#include "gaia_ping_pong.h"
+
+using namespace std;
+using namespace gaia::ping_pong;
+
+int main()
+{
+    gaia::system::initialize();
+    gaia_log::app().info("Ping Pong example is running...");
+
+    gaia::db::begin_transaction();
+    auto ping_pong_id = ping_pong_t::insert_row("pong");
+    auto ping_pong = ping_pong_t::get(ping_pong_id);
+
+    gaia::db::commit_transaction();
+
+    int count = 0;
+
+    while (true)
+    {
+        count++;
+        gaia::db::begin_transaction();
+        ping_pong = ping_pong_t::get(ping_pong_id);
+        if (strcmp(ping_pong.status(), "pong") == 0)
+        {
+            gaia_log::app().info("Main:{} iteration:{}", ping_pong.status(), count);
+            auto ping_pong_writer = ping_pong.writer();
+            ping_pong_writer.status = "ping";
+            ping_pong_writer.update_row();
+        }
+        gaia::db::commit_transaction();
+    }
+
+    gaia::system::shutdown();
+}
