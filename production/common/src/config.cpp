3c66e25da2909e14dd3690fa5c92030e0458cab0
Wayne Warren <wayne@warrenfamilyhome.com>
2021-02-02 14:46:26 -0800

    PR review updates

--- production/common/src/config.cpp
+++ production/common/src/config.cpp
@@ -15,8 +15,8 @@ namespace common
 
 bool file_exists(const char* filename)
 {
-    ifstream the_file(filename);
-    return static_cast<bool>(the_file);
+    ifstream file(filename);
+    return static_cast<bool>(file);
 }
 
 void error_if_not_exists(const char* filename)
@@ -29,8 +29,7 @@ void error_if_not_exists(const char* filename)
 
 string get_default_conf_file(const char* default_filename)
 {
-    // Default locations for log files are placed under/opt/gaia/etc/
-    static const char* c_default_conf_directory = "/opt/gaia/etc/";
+    // Default locations for log files are placed under /opt/gaia/etc/
     string str = c_default_conf_directory;
     str.append(default_filename);
 
@@ -48,17 +47,17 @@ string get_default_conf_file(const char* default_filename)
 
 // If the user passed in a filename, then throw an exception if it does not exist.
 // If the user did not pass in a filename, then look under /opt/gaia/etc/ for the
-// file and see if that exists.  If that file doesn't exist, then continue on
+// file and see if that exists.  If that file doesn't exist, then continue
 // without throwing an exception.  Components can initialize themselves with
 // appropriate defaults.
-string gaia::common::get_conf_file(const char* user_filename, const char* default_filename)
+string gaia::common::get_conf_file(const char* user_filepath, const char* default_filename)
 {
     string str;
 
-    if (user_filename)
+    if (user_filepath)
     {
-        error_if_not_exists(user_filename);
-        str = user_filename;
+        error_if_not_exists(user_filepath);
+        str = user_filepath;
     }
     else
     {
--- production/inc/gaia_internal/common/config.hpp
+++ production/inc/gaia_internal/common/config.hpp
@@ -13,13 +13,16 @@ namespace gaia
 namespace common
 {
 
+// Location where the configuration file will be copied during an SDK installation.
+constexpr char c_default_conf_directory[] = "/opt/gaia/etc/";
+
 constexpr char c_default_conf_file_name[] = "gaia.conf";
 
 constexpr char c_default_logger_conf_file_name[] = "gaia_log.conf";
 
 constexpr char c_conf_file_flag[] = "--configuration-file";
 
-std::string get_conf_file(const char* user_filename, const char* default_filename);
+std::string get_conf_file(const char* user_filepath, const char* default_filename);
 
 } // namespace common
 } // namespace gaia
