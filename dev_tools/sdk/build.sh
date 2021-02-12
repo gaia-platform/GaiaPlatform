392a45823c77d42506e512b80ecb12c354e14db5
Dax Hawkins <dax@gaiaplatform.io>
2021-02-12 06:52:24 -0800

    Initial outline of SDK CI build

---
+++ dev_tools/sdk/build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#############################################
+# Copyright (c) Gaia Platform LLC
+# All rights reserved.
+#############################################
+
+gaia_db_server &
+
+# Make incubator example.
+cd incubator
+mkdir build
+cd build
+cmake ..
+make 
+cd ../..
+
+# Make Hello World example.
+cd hello
+mkdir build
+cd build
+cmake ..
+make
+cd ../..
