f5ed778142ba7170c620039a2f021b75c385f12b
Chuan Liu <liuchuan@liuchuan.org>
2021-02-19 16:10:29 -0800

    Update fdw test to include arrays

--- production/sql/tests/test.sql
+++ production/sql/tests/test.sql
@@ -79,3 +79,39 @@ ORDER BY
     r1.dst_ap,
     r2.airline,
     r2.dst_ap;
+
+-- Array insertion
+INSERT INTO airport_fdw.passengers(name, miles_flown_by_quarter)
+VALUES('Jane Doe', ARRAY[1000,2000, 3000, 4000, 5000, 6000]);
+
+INSERT INTO airport_fdw.passengers(name, miles_flown_by_quarter)
+VALUES('Joe Public', ARRAY[500, 500, 1000, 1500, 2500, 4000]);
+
+-- Array selection
+SELECT
+    name, miles_flown_by_quarter
+FROM
+    airport_fdw.passengers
+ORDER BY
+    name;
+
+-- Array element selection
+SELECT
+    name, miles_flown_by_quarter[4]
+FROM
+    airport_fdw.passengers
+ORDER BY
+    name;
+
+-- Array update
+UPDATE airport_fdw.passengers
+SET miles_flown_by_quarter[6] = 2000
+WHERE name = 'Jane Doe';
+
+SELECT miles_flown_by_quarter[6]
+FROM airport_fdw.passengers
+WHERE name = 'Jane Doe';
+
+-- Array deletion
+DELETE FROM airport_fdw.passengers WHERE name = 'Jane Doe';
+SELECT name FROM airport_fdw.passengers;
