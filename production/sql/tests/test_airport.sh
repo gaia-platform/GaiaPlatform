6100cbe3e96452182ef164fc3d0c70274b7051b5
Laurentiu Cristofor <laurentiu@gaiaplatform.io>
2021-02-25 10:27:00 -0800

    FDW tests cleanup/refactoring

--- production/sql/tests/test_airport.sh
+++ production/sql/tests/test_airport.sh
@@ -8,10 +8,11 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-PG_CLIENT="$1"
-SETUP_SCRIPT="$2"
-TEST_SCRIPT="$3"
-EXPECTED_FILE="$4"
+DATA_DIR="$1"
+PG_CLIENT="$2"
+SETUP_SCRIPT="$3"
+TEST_SCRIPT="$4"
+EXPECTED_FILE="$5"
 
 # export PGHOST=${PGHOST-localhost}
 # export PGPORT=${PGPORT-5432}
@@ -19,8 +20,17 @@ EXPECTED_FILE="$4"
 export PGUSER=${PGUSER-postgres}
 # export PGPASSWORD=
 
+# Unzip data files to a folder under /tmp.
+TMP_DIR=$(mktemp -d -t airport-data-XXXXXXXXXX)
+chmod 755 "$TMP_DIR"
+pushd "$DATA_DIR"
+for f in *gz; do
+    gzip -dkc < $f > "$TMP_DIR/${f%%.gz}"
+done
+popd
+
 # Set up database tables and import airport data.
-"$PG_CLIENT" -f "$SETUP_SCRIPT"
+"$PG_CLIENT" --set=data_dir="$TMP_DIR" -f "$SETUP_SCRIPT"
 
 # Execute test queries.
 OUTPUT=$("$PG_CLIENT" -A -F , -X -t -f "$TEST_SCRIPT")
--- production/sql/tests/test_array.sql
+++ production/sql/tests/test_array.sql
@@ -3,20 +3,20 @@
 -- All rights reserved.
 ---------------------------------------------
 
-\c airport;
+\c test_array;
 
 -- Array insertion
-INSERT INTO airport_fdw.passengers(name, miles_flown_by_quarter)
+INSERT INTO test_fdw.passengers(name, miles_flown_by_quarter)
 VALUES('Jane Doe', ARRAY[1000,2000, 3000, 4000, 5000, 6000]);
 
-INSERT INTO airport_fdw.passengers(name, miles_flown_by_quarter)
+INSERT INTO test_fdw.passengers(name, miles_flown_by_quarter)
 VALUES('Joe Public', ARRAY[500, 500, 1000, 1500, 2500, 4000]);
 
 -- Array selection
 SELECT
     name, miles_flown_by_quarter
 FROM
-    airport_fdw.passengers
+    test_fdw.passengers
 ORDER BY
     name;
 
@@ -24,19 +24,19 @@ ORDER BY
 SELECT
     name, miles_flown_by_quarter[4]
 FROM
-    airport_fdw.passengers
+    test_fdw.passengers
 ORDER BY
     name;
 
 -- Array update
-UPDATE airport_fdw.passengers
+UPDATE test_fdw.passengers
 SET miles_flown_by_quarter[6] = 2000
 WHERE name = 'Jane Doe';
 
 SELECT miles_flown_by_quarter[6]
-FROM airport_fdw.passengers
+FROM test_fdw.passengers
 WHERE name = 'Jane Doe';
 
 -- Array deletion
-DELETE FROM airport_fdw.passengers WHERE name = 'Jane Doe';
-SELECT name FROM airport_fdw.passengers;
+DELETE FROM test_fdw.passengers WHERE name = 'Jane Doe';
+SELECT name FROM test_fdw.passengers;
