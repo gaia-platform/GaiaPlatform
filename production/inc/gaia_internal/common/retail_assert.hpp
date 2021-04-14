935e7d18fb4e35c27dd0494c66d4eaa0f27f0184
Laurentiu Cristofor <laurentiu@gaiaplatform.io>
2021-04-13 19:50:16 -0700

    retail_assert improvements

--- production/inc/gaia_internal/common/retail_assert.hpp
+++ production/inc/gaia_internal/common/retail_assert.hpp
@@ -5,6 +5,7 @@
 
 #pragma once
 
+#include <sstream>
 #include <string>
 
 #include "gaia/exception.hpp"
@@ -22,6 +23,25 @@ namespace common
  * @{
  */
 
+#ifdef DISABLE_RETAIL_ASSERTS
+#define retail_assert(c, m) no_op()
+#else
+#define retail_assert(c, m) retail_assert_do_not_call_directly(c, m, __FILE__, __LINE__, __func__)
+#endif
+
+/**
+ * A no-operation function.
+ *
+ * When we want to disable retail_assert calls, we'll call this function instead,
+ * so that these calls will be optimized out by the compiler.
+ *
+ * We need a function because retail_assert calls can be prefixed with a namespace
+ * qualifier and those qualifiers have to precede a function call.
+ */
+inline void no_op()
+{
+}
+
 /**
  * Thrown when a retail assert check has failed.
  */
@@ -37,12 +57,18 @@ public:
 /**
  * Retail asserts are meant for important conditions that indicate internal errors.
  * They help us surface issues early, at the source.
+ *
+ * This function should only be called through the retail_assert macro,
+ * so that it gets passed the correct information about the point of call.
  */
-inline void retail_assert(bool condition, const std::string& message)
+inline void retail_assert_do_not_call_directly(
+    bool condition, const std::string& message, const char* file, size_t line, const char* function)
 {
     if (!condition)
     {
-        throw retail_assertion_failure(message);
+        std::stringstream message_stream;
+        message_stream << message << "\nFile: " << file << ".\nLine: " << line << ".\nFunction: " << function << ".";
+        throw retail_assertion_failure(message_stream.str());
     }
 }
 
