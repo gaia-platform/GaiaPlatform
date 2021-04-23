4115623dd682e31a3039f58071eca66679760a75
Laurentiu Cristofor <laurentiu.cristofor@outlook.com>
2021-04-23 13:09:08 -0700

    Small test changes (#635)
    Co-authored-by: Laurentiu Cristofor <laurentiu@gaiaplatform.io>

--- scratch/simone/str_concat_comparison/src/main.cpp
+++ scratch/simone/str_concat_comparison/src/main.cpp
@@ -23,6 +23,8 @@ int main(int argc, char* argv[])
 
     int sizes[num_iter];
 
+    int total_size = 0;
+
     std::chrono::steady_clock::time_point start;
 
     std::cout << "Num Iter: " << num_iter << std::endl;
@@ -30,36 +32,68 @@ int main(int argc, char* argv[])
     start = gaia_timer_t::get_time_point();
     for (int i = 0; i < num_iter; i++)
     {
-        std::string final = fmt::format("{}_{}_{}", str1, str2, i);
+        std::string final = fmt::format("{}_{}_{}_{}", i, str1, i, str2);
         sizes[i] = final.size();
+        // std::cout << final << std::endl;
     }
     gaia_timer_t::log_duration(start, "fmt");
+
+    total_size = 0;
+    for (int i = 0; i < num_iter; i++)
+    {
+        total_size += sizes[i];
+    }
+    std::cout << "Concatenated " << total_size << " characters" << std::endl;
 
     start = gaia_timer_t::get_time_point();
     for (int i = 0; i < num_iter; i++)
     {
         std::ostringstream ss;
-        ss << str1 << "_" << str2 << "_" << i;
+        ss << i << "_" << str1 << "_" << i << "_" << str2;
         std::string final = ss.str();
         sizes[i] = final.size();
+        // std::cout << final << std::endl;
     }
     gaia_timer_t::log_duration(start, "ostringstream");
+
+    total_size = 0;
+    for (int i = 0; i < num_iter; i++)
+    {
+        total_size += sizes[i];
+    }
+    std::cout << "Concatenated " << total_size << " characters" << std::endl;
 
     start = gaia_timer_t::get_time_point();
     for (int i = 0; i < num_iter; i++)
     {
         std::stringstream ss;
-        ss << str1 << "_" << str2 << "_" << i;
+        ss << i << "_" << str1 << "_" << i << "_" << str2;
         std::string final = ss.str();
         sizes[i] = final.size();
+        // std::cout << final << std::endl;
     }
     gaia_timer_t::log_duration(start, "stringstream");
+
+    total_size = 0;
+    for (int i = 0; i < num_iter; i++)
+    {
+        total_size += sizes[i];
+    }
+    std::cout << "Concatenated " << total_size << " characters" << std::endl;
 
     start = gaia_timer_t::get_time_point();
     for (int i = 0; i < num_iter; i++)
     {
-        std::string final = str1 + "_" + str2 + "_" + std::to_string(i);
+        std::string final = std::to_string(i) + "_" + str1 + "_" + std::to_string(i) + "_" + str2;
         sizes[i] = final.size();
+        // std::cout << final << std::endl;
     }
     gaia_timer_t::log_duration(start, "string concat");
+
+    total_size = 0;
+    for (int i = 0; i < num_iter; i++)
+    {
+        total_size += sizes[i];
+    }
+    std::cout << "Concatenated " << total_size << " characters" << std::endl;
 }
