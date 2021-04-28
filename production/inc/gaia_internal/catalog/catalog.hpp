21040446a57c811f6e536a99af1bf85a9d5efaec
Chuan Liu <liuchuan@liuchuan.org>
2021-04-28 08:57:44 -0700

    Address CR comments

--- production/inc/gaia_internal/catalog/catalog.hpp
+++ production/inc/gaia_internal/catalog/catalog.hpp
@@ -263,10 +263,10 @@ struct drop_statement_t : statement_t
 /*@}*/
 } // namespace ddl
 
-class no_system_db_operation : public gaia::common::gaia_exception
+class forbidden_sydtem_db_operation : public gaia::common::gaia_exception
 {
 public:
-    explicit no_system_db_operation(const std::string& name)
+    explicit forbidden_sydtem_db_operation(const std::string& name)
     {
         m_message = "Operations on the system database '" + name + "' are not allowed.";
     }
