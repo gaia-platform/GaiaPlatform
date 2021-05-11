3c69a058371d9eca4aabcdcc5ff37ffbe6016df1
Ubuntu <mihir@gaiaplatform.com>
2021-05-11 15:37:36 +0000

    Wal file

---
+++ production/db/core/inc/wal_file.hpp
@@ -0,0 +1,50 @@
+/////////////////////////////////////////////
+// Copyright (c) Gaia Platform LLC
+// All rights reserved.
+/////////////////////////////////////////////
+
+#pragma once
+
+#include <fcntl.h>
+#include <unistd.h>
+
+#include <cstddef>
+
+#include <atomic>
+#include <memory>
+#include <string>
+
+namespace gaia
+{
+namespace db
+{
+
+typedef uint32_t wal_sequence_t;
+typedef uint32_t wal_file_offset_t;
+
+// Layer to help carry out async I/O on a file via io_uring.
+// Note that all ring management operations (has_enough_size(); if not, submit the batch) occur in wal_writer.cpp
+class wal_file_t
+{
+
+public:
+    static uint64_t file_size;
+    static wal_sequence_t file_num;
+    static wal_file_offset_t current_offset;
+    static std::string dir_name;
+    static int dir_fd;
+    static int file_fd;
+
+    wal_file_t(std::string& dir_name, int dir_fd, wal_sequence_t file_num, size_t file_size);
+    ~wal_file_t();
+
+    bool has_enough_space(size_t record_size);
+    wal_file_offset_t get_current_offset();
+    void allocate(size_t size);
+
+private:
+    void validate_record_size(size_t record_size);
+};
+
+} // namespace db
+} // namespace gaia
---
+++ production/db/core/src/wal_file.cpp
@@ -0,0 +1,85 @@
+/////////////////////////////////////////////
+// Copyright (c) Gaia Platform LLC
+// All rights reserved.
+/////////////////////////////////////////////
+
+#include "wal_file.hpp"
+
+#include <fcntl.h>
+#include <unistd.h>
+
+#include <cstddef>
+
+#include <atomic>
+#include <memory>
+#include <sstream>
+#include <string>
+
+#include "gaia_internal/db/db_types.hpp"
+
+#include "db_internal_types.hpp"
+// #include "io_uring_error.hpp"
+#include "gaia_internal/common/retail_assert.hpp"
+#include "gaia_internal/common/write_ahead_log_error.hpp"
+
+#include "liburing.h"
+
+using namespace gaia::common;
+using namespace gaia::db;
+
+// Todo: Pipe calls to an I/O uring submission queue instead of performing synchronous calls to open,
+// close and fallocate the file.
+wal_file_t::wal_file_t(std::string& dir, int fd, wal_sequence_t file_seq, size_t size)
+{
+    dir_fd = fd;
+    dir_name = dir;
+    file_num = file_seq;
+    file_size = size;
+
+    // Open and fallocate depending on size.
+    std::stringstream file_name;
+    file_name << dir_name << "/" << file_num;
+    file_fd = open(file_name.str().c_str(), O_WRONLY | O_CREAT);
+    if (file_fd == -1)
+    {
+        throw write_ahead_log_error("Unable to create wal file");
+    }
+
+    // Todo: zero-fill entires in file.
+    // Reference: http://yoshinorimatsunobu.blogspot.com/2009/05/overwriting-is-much-faster-than_28.html
+    int err = fallocate(file_fd, 0, 0, file_size);
+
+    if (err == -1)
+    {
+        int err = errno;
+        throw write_ahead_log_error("fallocate call for wal file failed", err);
+    }
+
+    // Calling fsync() does not necessarily ensure that the entry in the directory containing
+    // the file has also reached disk. For that an explicit fsync() on a file descriptor
+    // for the directory is also needed.
+    fsync(file_fd);
+    fsync(dir_fd);
+}
+
+wal_file_t::~wal_file_t()
+{
+    // Close file.
+    // TODO(mihir) submit batch before close.
+    close(file_fd);
+}
+
+wal_file_offset_t wal_file_t::get_current_offset()
+{
+    return current_offset;
+}
+
+void wal_file_t::allocate(size_t size)
+{
+    current_offset += size;
+}
+
+bool wal_file_t::has_enough_space(size_t record_size)
+{
+    return current_offset + record_size < file_size;
+}
