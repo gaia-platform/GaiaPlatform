a10531bca6bfb3ec79530fca2068fe580b4078bb
gregory <gregory@gaiaplatform.io>
2021-05-27 11:30:46 -0700

    Fix a code generation issue

--- production/tools/gaia_translate/inc/table_navigation.h
+++ production/tools/gaia_translate/inc/table_navigation.h
@@ -61,7 +61,7 @@ public:
         return m_table_data;
     }
     // Function that generates code to navigate between tables for explicit navigation.
-    static navigation_code_data_t generate_explicit_navigation_code(string anchor_table, explicit_path_data_t path_data);
+    static navigation_code_data_t generate_explicit_navigation_code(const string& anchor_table, explicit_path_data_t path_data);
     // Function that generates variable name for navigation code variables.
     static string get_variable_name(const string& table, const unordered_map<string, string>& tags);
 private:
@@ -99,8 +99,8 @@ private:
     static string get_closest_table(const unordered_map<string, int>& table_distance);
     static bool find_navigation_path(const string& src, const string& dst, vector<navigation_data_t>& current_path);
     static string generate_random_string(string::size_type length);
-    static navigation_code_data_t generate_navigation_code(string anchor_table, unordered_set<string> tables,
-        unordered_map<string, string> tags,  string& last_table);
+    static navigation_code_data_t generate_navigation_code(const string& anchor_table, const unordered_set<string>& tables,
+        const unordered_map<string, string>& tags,  string& last_table);
     static bool generate_navigation_step(const string& source_table, const string& source_field, const string& destination_table,
         const string& source_variable_name, const string& variable_name, navigation_code_data_t& navigation_data);
     static bool m_is_initialized;
