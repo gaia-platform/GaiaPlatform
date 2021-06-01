238a10813b4bf6432331182fc4b53a12c831fdfd
yiwen-wong <67293355+yiwen-wong@users.noreply.github.com>
2021-06-01 10:22:51 -0700

    INDEX: upgrade data_holder_t reflection for index keys (#674)
    * INDEX: upgrade data_holder_t reflection for index keys
    
    * Addressed review comments
    
    * Fixed spacing and added tests

--- production/db/inc/payload_types/data_holder.hpp
+++ production/db/inc/payload_types/data_holder.hpp
@@ -8,7 +8,13 @@
 #include <cstddef>
 #include <cstdint>
 
+#include <ostream>
+#include <string_view>
+#include <type_traits>
+
 #include "flatbuffers/reflection.h"
+
+#include "gaia/exception.hpp"
 
 namespace gaia
 {
@@ -17,8 +23,21 @@ namespace db
 namespace payload_types
 {
 
+// We are using std::size_t for no other reason than std::hash() does the same.
+typedef std::size_t data_hash_t;
+
+class unboxing_error : public gaia::common::gaia_exception
+{
+public:
+    explicit unboxing_error(const std::string& message)
+    {
+        m_message = message;
+    }
+};
+
 // Return true if the type is a signed integer type.
-inline bool is_signed_integer(reflection::BaseType type)
+inline bool
+is_signed_integer(reflection::BaseType type)
 {
     // Signed integer types have odd BaseType enum values.
     // This logic can break if flatbuffers adds support
@@ -35,6 +54,7 @@ union value_holder_t
     int64_t integer_value;
     double float_value;
     const char* string_value;
+    std::string_view vector_value{};
 };
 
 // A simple structure that isolates us from the details
@@ -45,12 +65,45 @@ struct data_holder_t
     value_holder_t hold;
 
     data_holder_t();
+    data_holder_t(const data_holder_t&) = default;
+    data_holder_t(data_holder_t&&) = default;
+
+    // Convenience ctors to allow implicit conversion from native types.
+    template <typename T>
+    data_holder_t(T value, typename std::enable_if_t<std::is_integral_v<T> && std::is_signed_v<T>>* = nullptr)
+    {
+        type = reflection::Int;
+        hold.integer_value = value;
+    }
+    template <typename T>
+    data_holder_t(T value, typename std::enable_if_t<std::is_integral_v<T> && !std::is_signed_v<T>>* = nullptr)
+    {
+        type = reflection::UInt;
+        hold.integer_value = value;
+    }
+    data_holder_t(float value);
+    data_holder_t(double value);
+    data_holder_t(const char* value);
+    data_holder_t(const char* value, size_t len);
 
     void clear();
 
     int compare(const data_holder_t& other) const;
+    data_hash_t hash() const;
+
+    // Convenience implicit conversions to native types.
+    operator int32_t() const;
+    operator uint32_t() const;
+    operator uint64_t() const;
+    operator int64_t() const;
+    operator float() const;
+    operator double() const;
+    operator const char*() const;
+
+    // For pretty-print/debugging.
+    friend std::ostream& operator<<(std::ostream& os, const data_holder_t& data);
 };
 
-}
-}
-}
+} // namespace payload_types
+} // namespace db
+} // namespace gaia
--- production/db/payload_types/src/data_holder.cpp
+++ production/db/payload_types/src/data_holder.cpp
@@ -5,6 +5,9 @@
 
 #include "data_holder.hpp"
 
+#include <functional>
+#include <string_view>
+
 #include "gaia_internal/common/retail_assert.hpp"
 
 #include "field_access.hpp"
@@ -23,6 +26,100 @@ data_holder_t::data_holder_t()
     clear();
 }
 
+data_holder_t::data_holder_t(float value)
+{
+    type = reflection::Float;
+    hold.float_value = value;
+}
+
+data_holder_t::data_holder_t(double value)
+{
+    type = reflection::Double;
+    hold.float_value = value;
+}
+
+data_holder_t::data_holder_t(const char* value)
+{
+    type = reflection::String;
+    hold.string_value = value;
+}
+
+data_holder_t::data_holder_t(const char* value, std::size_t len)
+{
+    type = reflection::Vector;
+    hold.vector_value = {value, len};
+}
+
+data_holder_t::operator uint64_t() const
+{
+    if (!flatbuffers::IsInteger(type) || is_signed_integer(type))
+    {
+        throw unboxing_error("Unbox failed: type needs to be unsigned integer.");
+    }
+    auto integer_ptr = reinterpret_cast<const uint64_t*>(&hold.integer_value);
+    return *integer_ptr;
+}
+
+data_holder_t::operator int64_t() const
+{
+    if (!is_signed_integer(type))
+    {
+        throw unboxing_error("Unbox failed: type needs to be signed integer.");
+    }
+    return hold.integer_value;
+}
+
+data_holder_t::operator uint32_t() const
+{
+    if (!flatbuffers::IsInteger(type) || is_signed_integer(type))
+    {
+        throw unboxing_error("Unbox failed: type needs to be unsigned integer.");
+    }
+    auto integer_ptr = reinterpret_cast<const uint64_t*>(&hold.integer_value);
+    return static_cast<uint32_t>(*integer_ptr);
+}
+
+data_holder_t::operator int32_t() const
+{
+    if (!is_signed_integer(type))
+    {
+        throw unboxing_error("Unbox failed: type needs to be signed integer.");
+    }
+    return static_cast<int32_t>(hold.integer_value);
+}
+
+data_holder_t::operator float() const
+{
+    if (!flatbuffers::IsFloat(type))
+    {
+        throw unboxing_error("Unbox failed: type needs to be float.");
+    }
+    return static_cast<float>(hold.float_value);
+}
+
+data_holder_t::operator double() const
+{
+    if (!flatbuffers::IsFloat(type))
+    {
+        throw unboxing_error("Unbox failed: type needs to be float.");
+    }
+    return hold.float_value;
+}
+
+data_holder_t::operator const char*() const
+{
+    if (type != reflection::String && type != reflection::Vector)
+    {
+        throw unboxing_error("Unbox failed: type needs to be string or vector.");
+    }
+
+    if (type == reflection::String)
+    {
+        return hold.string_value;
+    }
+    return hold.vector_value.data();
+}
+
 void data_holder_t::clear()
 {
     type = reflection::None;
@@ -33,22 +130,7 @@ int data_holder_t::compare(const data_holder_t& other) const
 {
     ASSERT_PRECONDITION(type == other.type, "data_holder_t::compare() was called for different types.");
 
-    if (type == reflection::String)
-    {
-        if (hold.string_value == nullptr && other.hold.string_value == nullptr)
-        {
-            return 0;
-        }
-        else if (hold.string_value == nullptr || other.hold.string_value == nullptr)
-        {
-            return (hold.string_value == nullptr) ? -1 : 1;
-        }
-        else
-        {
-            return strcmp(hold.string_value, other.hold.string_value);
-        }
-    }
-    else if (flatbuffers::IsInteger(type))
+    if (flatbuffers::IsInteger(type))
     {
         if (is_signed_integer(type))
         {
@@ -75,10 +157,126 @@ int data_holder_t::compare(const data_holder_t& other) const
             ? 0
             : (hold.float_value > other.hold.float_value) ? 1 : -1;
     }
+    else if (type == reflection::String)
+    {
+        if (hold.string_value == nullptr && other.hold.string_value == nullptr)
+        {
+            return 0;
+        }
+        else if (hold.string_value == nullptr || other.hold.string_value == nullptr)
+        {
+            return (hold.string_value == nullptr) ? -1 : 1;
+        }
+        else
+        {
+            return strcmp(hold.string_value, other.hold.string_value);
+        }
+    }
+    else if (type == reflection::Vector)
+    {
+        if (hold.vector_value.data() == nullptr && other.hold.vector_value.data() == nullptr)
+        {
+            return 0;
+        }
+        else if (hold.vector_value.data() == nullptr || other.hold.vector_value.data() == nullptr)
+        {
+            return (hold.vector_value.data() == nullptr) ? -1 : 1;
+        }
+        else
+        {
+            // Perform lexicographic compare. This treats vectors as BLOBs, allowing vector
+            // to be compared regardless of length.
+            //
+            // This means APIs using compare() cannot perform introspection, but this provides a path
+            // for generic code to handle Vector types.
+
+            size_t len = std::min(other.hold.vector_value.size(), hold.vector_value.size());
+            int cmp = memcmp(hold.vector_value.data(), other.hold.vector_value.data(), len);
+
+            if (cmp == 0 && hold.vector_value.size() != other.hold.vector_value.size())
+            {
+                return (hold.vector_value.size() < other.hold.vector_value.size()) ? -1 : 1;
+            }
+
+            return cmp;
+        }
+    }
     else
     {
         throw unhandled_field_type(type);
     }
+}
+
+std::size_t data_holder_t::hash() const
+{
+    if (flatbuffers::IsInteger(type))
+    {
+        return std::hash<int64_t>{}(hold.integer_value);
+    }
+    else if (flatbuffers::IsFloat(type))
+    {
+        return std::hash<double>{}(hold.float_value);
+    }
+    else if (type == reflection::String)
+    {
+        if (hold.string_value == nullptr)
+        {
+            return 0;
+        }
+        else
+        {
+            return std::hash<std::string_view>{}(hold.string_value);
+        }
+    }
+    else if (type == reflection::Vector)
+    {
+        if (hold.vector_value.data() == nullptr)
+        {
+            return 0;
+        }
+        else
+        {
+            return std::hash<std::string_view>{}(hold.vector_value);
+        }
+    }
+    else
+    {
+        throw unhandled_field_type(type);
+    }
+}
+
+std::ostream& operator<<(std::ostream& os, const data_holder_t& data)
+{
+    if (flatbuffers::IsInteger(data.type))
+    {
+
+        if (is_signed_integer(data.type))
+        {
+            os << data.hold.integer_value;
+        }
+        else
+        {
+            os << *(reinterpret_cast<const uint64_t*>(&data.hold.integer_value));
+        }
+    }
+    else if (flatbuffers::IsFloat(data.type))
+    {
+        os << data.hold.float_value;
+    }
+    else if (data.type == reflection::String)
+    {
+        os << data.hold.string_value;
+    }
+    else if (data.type == reflection::Vector)
+    {
+        os << "<vector>";
+    }
+    else
+    {
+        throw unhandled_field_type(data.type);
+    }
+
+    return os;
 }
 
 } // namespace payload_types
