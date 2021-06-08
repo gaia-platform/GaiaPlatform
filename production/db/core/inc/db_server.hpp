f72b80613e86c20d283ec6c3345eb4b6e77aa769
Ubuntu <yiwen@gaiaplatform.com>
2021-06-08 16:40:21 -0700

    INDEX: index maintenance and recovery on server-side
    This reverts commit 63d4c5c1e9c38f22b90146237e2b1db1b0627ed4.

--- production/db/core/inc/db_server.hpp
+++ production/db/core/inc/db_server.hpp
@@ -253,6 +253,9 @@ private:
 
     static void init_shared_memory();
 
+    static void init_indexes();
+    static void update_indexes_from_log();
+
     static void create_local_snapshot(bool apply_logs);
 
     static void recover_db();
--- production/db/core/inc/type_generator.hpp
+++ production/db/core/inc/type_generator.hpp
@@ -34,6 +34,7 @@ private:
     common::gaia_type_t m_type;
     storage::record_iterator_t m_iterator;
     bool m_is_initialized;
+    bool m_manage_local_snapshot;
 };
 
 } // namespace db
