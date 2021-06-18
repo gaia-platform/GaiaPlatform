41d4750830c85934885336f2d785b08f67dac5d3
Ubuntu <yiwen@gaiaplatform.com>
2021-06-18 16:39:10 -0700

    Fix typo during merge

--- production/db/core/inc/db_helpers.hpp
+++ production/db/core/inc/db_helpers.hpp
@@ -150,7 +150,8 @@ inline void apply_logs_to_locators(locators_t* locators, txn_log_t* logs)
             (*locators)[lr.locator] = lr.new_offset;
         }
     }
-  
+}
+
 inline index::db_index_t id_to_index(common::gaia_id_t index_id)
 {
     auto it = get_indexes()->find(index_id);
