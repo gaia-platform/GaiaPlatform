29fd8fb57ece3180e408c8895e5966891c4ff3ec
Laurentiu Cristofor <laurentiu@gaiaplatform.io>
2021-06-22 16:03:33 -0700

    Add a multiple-producer single-consumer queue implementation

--- production/inc/gaia_internal/common/queue.hpp
+++ production/inc/gaia_internal/common/queue.hpp
@@ -40,6 +40,8 @@ struct queue_element_t
     queue_element_t(T value);
 };
 
+// This is a general-purpose queue implementation
+// with synchronization performed using mutexes at node level.
 template <class T>
 class queue_t
 {
@@ -70,6 +72,54 @@ protected:
     // or false if it was aborted due to another concurrent operation.
     // In the latter case, the caller should retry the operation again.
     bool dequeue_internal(T& value);
+};
+
+template <class T>
+struct mpsc_queue_node_t
+{
+    mpsc_queue_node_t* volatile next;
+    T value;
+
+    mpsc_queue_node_t();
+    mpsc_queue_node_t(T value);
+};
+
+// This implementation of a multiple-producer single-consumer (mpsc) queue
+// is based on the implementation described at:
+// https://www.1024cores.net/home/lock-free-algorithms/queues/non-intrusive-mpsc-node-based-queue
+template <class T>
+class mpsc_queue_t
+{
+public:
+    mpsc_queue_t();
+    ~mpsc_queue_t();
+
+    // Insert a value in the queue.
+    void enqueue(const T& value);
+
+    // Extract a value from the queue.
+    // If the queue is empty, the value will be left unset.
+    // The caller should properly initialize the value before this call,
+    // to be able to determine if it was actually set.
+    void dequeue(T& value);
+
+    inline bool is_empty() const;
+
+    inline size_t size() const;
+
+protected:
+    void enqueue_internal(mpsc_queue_node_t<T>* node);
+    mpsc_queue_node_t<T>* dequeue_internal();
+
+protected:
+    std::atomic<mpsc_queue_node_t<T>*> volatile m_head;
+    mpsc_queue_node_t<T>* m_tail;
+
+    // The queue structure is guaranteed to never actually be empty
+    // by having it contain references to this "stub" node when it is logically empty.
+    mpsc_queue_node_t<T> m_stub;
+
+    std::atomic<size_t> m_size;
 };
 
 #include "queue.inc"
