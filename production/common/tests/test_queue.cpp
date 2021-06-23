0fa1546033abad9651a4921b2afa79f4268d1774
Laurentiu Cristofor <laurentiu.cristofor@outlook.com>
2021-06-23 14:42:44 -0700

    Add a multiple-producer single-consumer (MPSC) queue implementation (#753)
    * Add a multiple-producer single-consumer queue implementation
    
    * Use mpsc_queue_t instead of queue_t
    
    * Add mpsc_queue code licenses
    
    * Add README file for new licenses folder
    
    * Remove volatile from atomic variable
    
    Co-authored-by: Laurentiu Cristofor <laurentiu@gaiaplatform.io>

--- production/common/tests/test_queue.cpp
+++ production/common/tests/test_queue.cpp
@@ -67,3 +67,59 @@ TEST(common, queue)
     ASSERT_EQ(0, value);
     ASSERT_EQ(0, queue.size());
 }
+
+TEST(common, mpsc_queue)
+{
+    mpsc_queue_t<int> queue;
+
+    int value = -1;
+
+    // Verify that the queue is empty.
+    queue.dequeue(value);
+    ASSERT_EQ(-1, value);
+    ASSERT_EQ(0, queue.size());
+
+    // Insert two values.
+    queue.enqueue(1);
+    queue.enqueue(2);
+    ASSERT_EQ(2, queue.size());
+
+    // Extract a value.
+    queue.dequeue(value);
+    ASSERT_EQ(1, value);
+    ASSERT_EQ(1, queue.size());
+
+    // Insert another value.
+    queue.enqueue(3);
+    ASSERT_EQ(2, queue.size());
+
+    // Extract the two values.
+    queue.dequeue(value);
+    ASSERT_EQ(2, value);
+    ASSERT_EQ(1, queue.size());
+
+    queue.dequeue(value);
+    ASSERT_EQ(3, value);
+    ASSERT_EQ(0, queue.size());
+
+    // Verify that the queue is empty again.
+    value = -1;
+    queue.dequeue(value);
+    ASSERT_EQ(-1, value);
+    ASSERT_EQ(0, queue.size());
+
+    // Insert one more value.
+    queue.enqueue(4);
+    ASSERT_EQ(1, queue.size());
+
+    // Extract the value.
+    queue.dequeue(value);
+    ASSERT_EQ(4, value);
+    ASSERT_EQ(0, queue.size());
+
+    // Verify that the queue is empty again.
+    value = 0;
+    queue.dequeue(value);
+    ASSERT_EQ(0, value);
+    ASSERT_EQ(0, queue.size());
+}
