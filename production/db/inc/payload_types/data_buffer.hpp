99372ef11cf29719647d08729a4c9884383b885d
Yiwen Wong <yiwen@gaiaplatform.com>
2021-08-16 15:05:06 -0700

    Added some comments.

--- production/db/inc/payload_types/data_buffer.hpp
+++ production/db/inc/payload_types/data_buffer.hpp
@@ -22,11 +22,14 @@ using serialization_buffer_t = flatbuffers::Vector<serialization_byte_t>;
 using serialization_write_buffer_t = std::vector<serialization_byte_t>;
 using serialization_output_t = flatbuffers::Offset<flatbuffers::Vector<serialization_byte_t>>;
 
-// These classes provide serialization/deserialization interfaces over flatbuffers type [byte].
+// These classes provide serialization/deserialization interfaces over the flatbuffers type [byte]
+// in Flatbuffer messages.
 
+// data_write_buffer_t provides a write() interface over a FlatBufferBuilder.
 class data_write_buffer_t
 {
 public:
+    // builder - The FlatBufferBuilder that holds the data needed for our final FlatBuffer object.
     explicit data_write_buffer_t(flatbuffers::FlatBufferBuilder& builder);
     data_write_buffer_t(const data_write_buffer_t& other) = delete;
 
@@ -41,6 +44,7 @@ private:
     flatbuffers::FlatBufferBuilder& m_builder;
 };
 
+// data_read_buffer_t provides read() interface over the [byte] field from a FlatBuffer object.
 class data_read_buffer_t
 {
 public:
