72646a78d0eb450dbc12f49886e84a5eee824e50
Simone Rondelli <simone@gaiaplatform.io>
2021-08-15 21:07:12 -0400

    Remove AST checks, fix bug

--- third_party/production/TranslationEngineLLVM/clang/test/Parser/gaia_connect_disconnect.cpp
+++ third_party/production/TranslationEngineLLVM/clang/test/Parser/gaia_connect_disconnect.cpp
@@ -1,4 +1,4 @@
-// RUN: %clang_cc1 -fgaia-extensions -ast-dump -verify %s -verify-ignore-unexpected=note | FileCheck -strict-whitespace %s
+// RUN: %clang_cc1 -fgaia-extensions -ast-dump -verify %s -verify-ignore-unexpected=note
 
 #include "barn_storage/gaia_barn_storage.h"
 
@@ -9,33 +9,14 @@ ruleset test_connect_disconnect_on_table
         for (/r : raised)
         {
             farmer.connect(r);
-            // CHECK:    CXXMemberCallExpr 0x{{[^ ]*}} <{{.*}}> 'bool'
-            // CHECK-NEXT:    MemberExpr 0x{{[^ ]*}} <{{.*}}> '<bound member function type>' .connect 0x{{[^ ]*}}
-            // CHECK-NEXT:    DeclRefExpr 0x{{[^ ]*}} <{{.*}}> 'farmer__type' lvalue Var 0x{{[^ ]*}} 'farmer' 'farmer__type'
-
             farmer.disconnect(r);
-            // CHECK:    CXXMemberCallExpr 0x{{[^ ]*}} <{{.*}}> 'bool'
-            // CHECK-NEXT:    MemberExpr 0x{{[^ ]*}} <{{.*}}> '<bound member function type>' .disconnect 0x{{[^ ]*}}
-            // CHECK-NEXT:    DeclRefExpr 0x{{[^ ]*}} <{{.*}}> 'farmer__type' lvalue Var 0x{{[^ ]*}} 'farmer' 'farmer__type'
         }
 
-        auto birthday = raised.insert(birthdate: "2 Aug 1990");
-        // CHECK: <{{.*}}>
-        farmer.connect(birthday);
-        // CHECK:    CXXMemberCallExpr 0x{{[^ ]*}} <{{.*}}> 'bool'
-        // CHECK-NEXT:    MemberExpr 0x{{[^ ]*}} <{{.*}}> '<bound member function type>' .connect 0x{{[^ ]*}}
-        // CHECK-NEXT:    DeclRefExpr 0x{{[^ ]*}} <{{.*}}> 'farmer__type' lvalue Var 0x{{[^ ]*}} 'farmer' 'farmer__type'
-        // CHECK-NEXT:    DeclRefExpr 0x{{[^ ]*}} <{{.*}}> 'raised__type':'raised__type' lvalue Var 0x{{[^ ]*}} 'birthday' 'raised__type':'raised__type'
-        farmer.disconnect(birthday);
-        // CHECK:    CXXMemberCallExpr 0x{{[^ ]*}} <{{.*}}> 'bool'
-        // CHECK-NEXT:    MemberExpr 0x{{[^ ]*}} <{{.*}}> '<bound member function type>' .disconnect 0x{{[^ ]*}}
-        // CHECK-NEXT:    DeclRefExpr 0x{{[^ ]*}} <{{.*}}> 'farmer__type' lvalue Var 0x{{[^ ]*}} 'farmer' 'farmer__type'
-        // CHECK-NEXT:    DeclRefExpr 0x{{[^ ]*}} <{{.*}}> 'raised__type':'raised__type' lvalue Var 0x{{[^ ]*}} 'birthday' 'raised__type':'raised__type'
-
-        // This won't work because of https://gaiaplatform.atlassian.net/browse/GAIAPLAT-1167
-        // the workaround is to have the raised_t type available.
         farmer.connect(raised.insert(birthdate: "2 Aug 1990"));
-        // CHECK: <{{.*}}>
+
+        auto birthday = raised.insert(birthdate: "2 Aug 1990");
+        farmer.connect(birthday);
+        farmer.disconnect(birthday);
     }
 }
 
@@ -46,16 +27,7 @@ ruleset test_connect_disconnect_on_link
         for (/i : incubator)
         {
             farmer.incubators.connect(i);
-            // CHECK:    CXXMemberCallExpr 0x{{[^ ]*}} <{{.*}}> 'bool'
-            // CHECK-NEXT:    MemberExpr 0x{{[^ ]*}} <{{.*}}> '<bound member function type>' .connect 0x{{[^ ]*}}
-            // CHECK-NEXT:    MemberExpr 0x{{[^ ]*}} <{{.*}}> 'farmer_incubators__type' lvalue .incubators 0x{{[^ ]*}}
-            // CHECK-NEXT:    DeclRefExpr 0x{{[^ ]*}} <{{.*}}> 'farmer__type' lvalue Var 0x{{[^ ]*}} 'farmer' 'farmer__type'
-
             farmer.incubators.disconnect(i);
-            // CHECK:    CXXMemberCallExpr 0x{{[^ ]*}} <{{.*}}> 'bool'
-            // CHECK-NEXT:    MemberExpr 0x{{[^ ]*}} <{{.*}}> '<bound member function type>' .disconnect 0x{{[^ ]*}}
-            // CHECK-NEXT:    MemberExpr 0x{{[^ ]*}} <{{.*}}> 'farmer_incubators__type' lvalue .incubators 0x{{[^ ]*}}
-            // CHECK-NEXT:    DeclRefExpr 0x{{[^ ]*}} <{{.*}}> 'farmer__type' lvalue Var 0x{{[^ ]*}} 'farmer' 'farmer__type'
 
             // This is not possible ATM because of this bug: https://gaiaplatform.atlassian.net/browse/GAIAPLAT-1037
             // In order to correctly tell apart tables from links we need to improve the parsing logic.
@@ -108,8 +80,8 @@ ruleset test_connect_disconnect_invalid_syntax_1
        crop.disconnect(); // expected-error {{no matching member function for call to 'disconnect'}}
        crop.yield.connect(); // expected-error {{too few arguments to function call, single argument 'param_1' was not specified}}
        crop.yield.disconnect(); // expected-error {{too few arguments to function call, single argument 'param_1' was not specified}}
-       crop.yield.connect("aaaaa"); // expected-error {{non-const lvalue reference to type 'yield__type' cannot bind to a value of unrelated type 'const char [6]'}}
-       crop.yield.disconnect(1); // expected-error {{non-const lvalue reference to type 'yield__type' cannot bind to a temporary of type 'int'}}
+       crop.yield.connect("aaaaa"); // expected-error {{reference to type 'const yield__type' could not bind to an lvalue of type 'const char [6]'}}
+       crop.yield.disconnect(1); // expected-error {{reference to type 'const yield__type' could not bind to an rvalue of type 'int'}}
     }
 }
 
