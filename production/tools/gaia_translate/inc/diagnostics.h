807d9e0dacdc8f5ffef54a8bbc7f1392ab571717
Dax Hawkins <dax@gaiaplatform.io>
2021-08-18 09:16:54 -0700

    Add instructions for how to add a diagnostic to gaiat

--- production/tools/gaia_translate/inc/diagnostics.h
+++ production/tools/gaia_translate/inc/diagnostics.h
@@ -10,6 +10,31 @@
 #include "clang/Basic/Diagnostic.h"
 #pragma clang diagnostic pop
 
+// When adding errors strings to the translation engine, please use the llvm
+// diagnostics infrastructure.  To add an error:
+//
+// 1) Check whether an appropriate error string already exists in DiagnosticsSemaKinds.td or DiagnosticParseKinds.td.
+// Our errors are at the bottom of the file grouped by category (error, notes, warn), and then alphabetically.  Note
+// the argument placeholders in the string (%0, %1).  If no error exists, feel free to add it.
+//
+// 2) Use 'gaiat::diag().emit(...)' to get the diagnostics engine and pass in the error string.  The IDs and other info
+// are generated from the DiagnosticsSemaKinds.td file and put into the 'diag' namespace.
+//
+// 3) You can set the source location to give the user line and column information about the error.  This can
+// be set on the global diag() object or overriden when you call the emit() method.  A typical pattern is to set
+// the source location on the diag() object at a top level function and then let lower level functions pass in
+// a better location if they have more specific information.
+//
+// As an example, to report "table 'foo' not found" you would write:
+// gaiat::emit(err_table_not_found) << table_name;
+//
+// To provide source location, you could write:
+// gaiat::emit(my_source_location, err_table_not_found) << table_name;
+//
+// Note that the arguments for place holders in the diagnostic string (%0, %1, ...) are provided from left to right:
+// gaiat::emit(err_two_params) << param_1 << param2;
+//
+
 namespace gaia
 {
 namespace translation
