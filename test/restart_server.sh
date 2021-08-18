d4a4bd2db1244107830dbffb5434f3bd276bf59f
Jack De Winter <jack@gaiaplatform.io>
2021-08-18 14:01:56 -0700

    Updates to test framework from recent debugging.

--- test/restart_server.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/bash
-
-#############################################
-# Copyright (c) Gaia Platform LLC
-# All rights reserved.
-#############################################
-
-
-sudo systemctl restart gaia
