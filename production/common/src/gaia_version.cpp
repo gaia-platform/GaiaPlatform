3b61e0c27acd5dac5ada0d5758560342a39d6403
Simone Rondelli <simone@gaiaplatform.io>
2021-08-23 17:31:17 -0400

    Add static assert to ensure the pre-release idetifier is added

--- production/common/src/gaia_version.cpp
+++ production/common/src/gaia_version.cpp
@@ -10,8 +10,19 @@ namespace gaia
 namespace common
 {
 
+template <std::size_t N>
+constexpr auto& assert_str_not_empty(char const (&s)[N])
+{
+    // There is a trailing `\0` in every literal, even if
+    // we don't explicitly specify it.
+    static_assert(N > 1, "String should not be empty!");
+    return s;
+}
+
 std::string gaia_version()
 {
+    assert_str_not_empty(c_gaia_pre_release);
+
     std::string version = std::to_string(c_gaia_version_major)
         + "." + std::to_string(c_gaia_version_minor)
         + "." + std::to_string(c_gaia_version_patch);
