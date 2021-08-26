e443603a891f6093bd18ed03bf96d49c3ae3cf16
mihirj1993 <61852742+mihirj1993@users.noreply.github.com>
2021-08-26 21:09:37 +0530

    Introduce iouring + persistent log file interface (#767)
    * Liburing gdev
    
    * Introduce IOUring & persistent_log_file
    
    * make m_batch_decisions private
    
    * use libexplain where possible
    
    * persistence namespace
    
    * Find static version of liburing
    
    * Update get_remaining_bytes_count() to return 0 if it doesn't have enough space to accomodate a new record

---
+++ production/db/core/inc/log_file.hpp
@@ -0,0 +1,64 @@
+/////////////////////////////////////////////
+// Copyright (c) Gaia Platform LLC
+// All rights reserved.
+/////////////////////////////////////////////
+
+#pragma once
+
+#include <fcntl.h>
+#include <unistd.h>
+
+#include <cstddef>
+
+#include <atomic>
+#include <memory>
+#include <string>
+
+#include "persistence_types.hpp"
+
+namespace gaia
+{
+namespace db
+{
+namespace persistence
+{
+
+/**
+ * Layer for persistent log file management.
+ */
+class log_file_t
+{
+public:
+    log_file_t(const std::string& dir_name, int dir_fd, size_t file_seq, size_t file_size);
+
+    /**
+     * Obtain offset to write the next log record at.
+     */
+    size_t get_current_offset();
+
+    /**
+     * Get remaining space in persistent log file.
+     */
+    size_t get_remaining_bytes_count(size_t record_size);
+
+    /**
+     * Allocate space in persistent log file.
+     */
+    void allocate(size_t size);
+
+    int get_file_fd();
+
+private:
+    size_t m_file_size;
+    size_t m_file_seq;
+    size_t m_current_offset;
+    std::string m_dir_name;
+    int m_dir_fd;
+    int m_file_fd;
+    std::string m_file_name;
+    inline static constexpr int c_file_permissions = 0666;
+};
+
+} // namespace persistence
+} // namespace db
+} // namespace gaia
