91a02771c2023aa1419b10ca2b12483358ad788d
Simone Rondelli <simone@gaiaplatform.io>
2021-08-27 19:59:41 -0400

    Address PR feedback

--- production/direct_access/tests/test_one_to_one.cpp
+++ production/direct_access/tests/test_one_to_one.cpp
@@ -7,8 +7,6 @@
 
 #include <gtest/gtest.h>
 
-#include "gaia/direct_access/auto_transaction.hpp"
-
 #include "gaia_internal/db/db_catalog_test_base.hpp"
 #include "gaia_internal/db/gaia_relationships.hpp"
 
