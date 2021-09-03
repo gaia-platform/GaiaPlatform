8dd1f392347e6ff5bc20ad8bbd85c361d768162f
Chuan Liu <liuchuan@liuchuan.org>
2021-09-02 21:26:39 -0700

    Update how cmake process_schema function handle db name

--- production/cmake/gaia_internal.cmake
+++ production/cmake/gaia_internal.cmake
@@ -145,8 +145,7 @@ endfunction()
 # - LIB_NAME: [optional] the name of the generated target.
 #     If not specified the default value is edc_${DDL_NAME}.
 # - DATABASE_NAME: [optional] name of the database the headers are generated from.
-#     If not specified the database name will be inferred as ${DDL_NAME}.
-#     This is a temporary workaround, until we improve gaiac.
+#     If not specified, the default database will be used.
 function(process_schema_internal)
   set(options "")
   set(oneValueArgs DDL_FILE OUTPUT_DIR LIB_NAME DATABASE_NAME INSTANCE_NAME)
@@ -157,57 +156,50 @@ function(process_schema_internal)
     message(FATAL_ERROR "You must specify either the DDL_FILE or the DATABASE_NAME!")
   endif()
 
-  # If the database name is not specified we infer it from the DDL file name.
-  if(NOT DEFINED ARG_DATABASE_NAME)
-    get_filename_component(DDL_NAME ${ARG_DDL_FILE} NAME)
-    string(REPLACE ".ddl" "" DDL_NAME ${DDL_NAME})
-    set(ARG_DATABASE_NAME ${DDL_NAME})
-    message(VERBOSE "DATABASE_NAME not specified, using: ${ARG_DATABASE_NAME}.")
-  endif()
-
   if(NOT DEFINED ARG_OUTPUT_DIR)
     set(ARG_OUTPUT_DIR ${GAIA_GENERATED_CODE}/${ARG_DATABASE_NAME})
     message(VERBOSE "OUTPUT_DIR not specified, defaulted to: ${ARG_OUTPUT_DIR}.")
   endif()
 
-  set(EDC_HEADER_FILE ${ARG_OUTPUT_DIR}/gaia_${ARG_DATABASE_NAME}.h)
-  set(EDC_CPP_FILE ${ARG_OUTPUT_DIR}/gaia_${ARG_DATABASE_NAME}.cpp)
-
   message(VERBOSE "Adding target to generate EDC code for database ${ARG_DATABASE_NAME}...")
 
   string(RANDOM GAIAC_INSTANCE_NAME)
 
+  set(GAIAC_COMMAND ${GAIA_PROD_BUILD}/catalog/gaiac/gaiac)
+  set(GAIAC_ARGS "-t" "${GAIA_PROD_BUILD}/db/core" "-o" "${ARG_OUTPUT_DIR}" "-n" "${GAIAC_INSTANCE_NAME}" "-g")
+
   if (DEFINED ARG_DDL_FILE)
-    add_custom_command(
-      COMMENT "Generating EDC code for file ${ARG_DDL_FILE} and database ${ARG_DATABASE_NAME}..."
-      OUTPUT ${EDC_HEADER_FILE}
-      OUTPUT ${EDC_CPP_FILE}
-      COMMAND ${GAIA_PROD_BUILD}/catalog/gaiac/gaiac
-        -t ${GAIA_PROD_BUILD}/db/core
-        -o ${ARG_OUTPUT_DIR}
-        -g ${ARG_DDL_FILE}
-        -d ${ARG_DATABASE_NAME}
-        -n ${GAIAC_INSTANCE_NAME}
-      DEPENDS ${ARG_DDL_FILE}
-      DEPENDS gaiac
-    )
-  else()
-    add_custom_command(
-      COMMENT "Generating EDC code for database ${ARG_DATABASE_NAME}..."
-      OUTPUT ${EDC_HEADER_FILE}
-      OUTPUT ${EDC_CPP_FILE}
-      COMMAND ${GAIA_PROD_BUILD}/catalog/gaiac/gaiac
-        -t ${GAIA_PROD_BUILD}/db/core
-        -o ${ARG_OUTPUT_DIR}
-        -d ${ARG_DATABASE_NAME}
-        -n ${GAIAC_INSTANCE_NAME}
-        -g
-      DEPENDS gaiac
-    )
+    message(STATUS "Adding target to load schema from the DDL file ${ARG_DDL_FILE}...")
+    list(APPEND GAIAC_ARGS ${ARG_DDL_FILE})
   endif()
 
+  if(DEFINED ARG_DATABASE_NAME)
+    set(EDC_HEADER_FILE ${ARG_OUTPUT_DIR}/gaia_${ARG_DATABASE_NAME}.h)
+    set(EDC_CPP_FILE ${ARG_OUTPUT_DIR}/gaia_${ARG_DATABASE_NAME}.cpp)
+    message(STATUS "Adding target to generate EDC code for database ${ARG_DATABASE_NAME}...")
+    list(PREPEND GAIAC_ARGS "-d" "${ARG_DATABASE_NAME}")
+  else()
+    # If the database name is not specified, we use the default database.
+    message(STATUS "DATABASE_NAME not specified, using default.")
+    set(EDC_HEADER_FILE ${ARG_OUTPUT_DIR}/gaia.h)
+    set(EDC_CPP_FILE ${ARG_OUTPUT_DIR}/gaia.cpp)
+  endif()
+
+  add_custom_command(
+    COMMENT "Generating EDC code..."
+    OUTPUT ${EDC_HEADER_FILE}
+    OUTPUT ${EDC_CPP_FILE}
+    COMMAND ${GAIAC_COMMAND} ${GAIAC_ARGS}
+    DEPENDS ${ARG_DDL_FILE}
+    DEPENDS gaiac
+  )
+
   if(NOT DEFINED ARG_LIB_NAME)
-    set(ARG_LIB_NAME "edc_${ARG_DATABASE_NAME}")
+    if (DEFINED ARG_DATABASE_NAME)
+      set(ARG_LIB_NAME "edc_${ARG_DATABASE_NAME}")
+    else()
+      set(ARG_LIB_NAME "edc")
+    endif()
     message(VERBOSE "LIB_NAME not specified, using: ${ARG_LIB_NAME}.")
   endif()
 
