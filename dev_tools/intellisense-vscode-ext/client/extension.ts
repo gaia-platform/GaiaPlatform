3274b82fd261c853b368bfb36759a5abd7446b9b
JC Fernandez <jcferna1@gmail.com>
2021-09-10 13:07:50 -0700

    removed unnecessary console logs and old comments.

--- dev_tools/intellisense-vscode-ext/client/extension.ts
+++ dev_tools/intellisense-vscode-ext/client/extension.ts
@@ -1,8 +1,5 @@
-// import { CppToolsApi, Version, CustomConfigurationProvider, getCppToolsApi } from 'vscode-cpptools';
 const vscode = require('vscode');
 const hover_info = require('./hover_info.json')
-
-//hover_info[property][word]
 
 function activate(context) {
 
@@ -12,10 +9,9 @@ function activate(context) {
         provideHover(document, position, token) {
             const range = document.getWordRangeAtPosition(position);
             const word = document.getText(range);
-            //function that constructs each hover based on its value in hover_info.json
+            //constructs each hover based on its value in ./hover_info.json
             for (const property in hover_info){
                 if (hover_info[property].hasOwnProperty(word)) {
-                    console.log('Working!')
                     return new vscode.Hover({
                         language: "ruleset",
                         value: hover_info[property][word]
