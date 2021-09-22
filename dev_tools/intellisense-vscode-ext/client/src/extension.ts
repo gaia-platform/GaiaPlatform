f80ce2bf64135fc65f5d3d17785835ebaaa23b8e
jc-gaiaplatform <jorge@gaiaplatform.io>
2021-09-22 11:51:54 -0700

    fixed spacing and comments

--- dev_tools/intellisense-vscode-ext/client/src/extension.ts
+++ dev_tools/intellisense-vscode-ext/client/src/extension.ts
@@ -55,21 +55,27 @@ import {
 let client: LanguageClient;
 
 function activate(context: ExtensionContext) {
-	//creates map out of hover_info keys/values
+
+	//creates map out of hover_info keys/values.
 	const hover_info_map = new Map(Object.entries(hover_info))
 
-	//creates provider to give the extension hover functionality
+	//creates provider to give the extension hover functionality.
     languages.registerHoverProvider('ruleset', {
         provideHover(document, position, token) {
-			//Finds the line that the user's cursor is hovering over
+
+			//Finds the line that the user's cursor is hovering over.
             const range = document.getWordRangeAtPosition(position);
-			//Gets the word that the cursor is hovering over given the line
+
+			//Gets the word that the cursor is hovering over given the line.
             const word: string = document.getText(range);
-            //constructs each hover based on its value in hover_info_map
+
+            //Constructs each hover based on its value in hover_info_map.
             if (hover_info_map.has(word)) {
-				//creates markdown string to be used in hover
+
+				//Creates markdown string to be used in hover.
 				let markdown_string = new MarkdownString(`${hover_info_map.get(word)}`)
-				//indicates that the markdown string is from a trusted source
+
+				//Indicates that the markdown string is from a trusted source.
 				markdown_string.isTrusted = true;
 >>>>>>> f837722ec (markdown strings for hovers)
                 return new Hover(markdown_string);
