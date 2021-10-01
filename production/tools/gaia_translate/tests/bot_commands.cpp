613712b923e796ed05c176d32992d6b32d26197f
Wayne Warren <wayne@warrenfamilyhome.com>
2021-10-01 16:51:55 -0700

    simplified the code - eliminated unused methods

--- production/tools/gaia_translate/tests/bot_commands.cpp
+++ production/tools/gaia_translate/tests/bot_commands.cpp
@@ -5,7 +5,15 @@
 
 #include "bot_commands.hpp"
 
-#include "bot_helpers.hpp"
+#include <string>
+#include <vector>
+
+#include "gaia/logger.hpp"
+#include "gaia/rules/rules.hpp"
+#include "gaia/system.hpp"
+
+#include "constants.hpp"
+#include "exceptions.hpp"
 
 using namespace gaia::common;
 using namespace gaia::db;
@@ -26,34 +34,9 @@ namespace amr_swarm
 namespace commands
 {
 
-// Constants.
-static constexpr char c_robot_topic_base[] = "bot";
-
-// static constexpr char c_topic_suffix_move_location[] = "move_location";
-// static constexpr char c_topic_suffix_status_request[] = "status_request";
-// static constexpr char c_topic_suffix_pickup_payload[] = "pickup_payload";
-// static constexpr char c_topic_suffix_drop_payload[] = "drop_payload";
-// static constexpr char c_topic_suffix_charge[] = "charge";
-
-// static constexpr char c_topic_unpack_pallet[] = "unpack_pallet";
-// static constexpr char c_topic_start_production[] = "start_production";
-// static constexpr char c_topic_unload_pl[] = "unload_pl";
-// static constexpr char c_topic_ship[] = "ship";
-// static constexpr char c_topic_receive_order[] = "receive_order";
-
-// static constexpr char c_status_empty_payload[] = " ";
-// static constexpr char c_status_request_payload[] = "charge_level";
-
 // Control whether the functions are expected to output a simple log message
 // detailing their request.
 bool show_outgoing_robot_messages = false;
-
-string create_bot_topic(const robot_t&, const string&)
-{
-    string topic(c_robot_topic_base);
-    topic += "communication::c_topic_separator + string(robot.sandbox_id()) + communication::c_topic_separator + sub_topic";
-    return topic;
-}
 
 void move_robot_to_station(const robot_t& robot, const gaia::amr_swarm::station_t station)
 {
@@ -88,7 +71,6 @@ void request_charge_level(const robot_t& robot)
     {
         gaia_log::app().info("BOT>>status_request: Requesting charge_level status for bot {}.", robot.id());
     }
-    // communication::publish_message(create_bot_topic(robot, c_topic_suffix_status_request), c_status_request_payload);
 }
 
 void pickup_pallet_from_station(const robot_t& robot, const gaia::amr_swarm::pallet_t pallet)
@@ -123,8 +105,6 @@ void pickup_pallet_from_station(const robot_t& robot, const gaia::amr_swarm::pal
     {
         gaia_log::app().info("BOT>>pickup_pallet: from station {} with robot {}.", robot.station().id(), robot.id());
     }
-
-    // communication::publish_message(create_bot_topic(robot, c_topic_suffix_pickup_payload), robot.station().sandbox_id());
 }
 
 void drop_pallet_at_station(const robot_t& robot)
@@ -145,24 +125,6 @@ void drop_pallet_at_station(const robot_t& robot)
     {
         gaia_log::app().info("BOT>>drop_pallet: robot {}.", robot.id());
     }
-
-    // communication::publish_message(create_bot_topic(robot, c_topic_suffix_drop_payload), robot.station().sandbox_id());
-}
-
-void start_charging_robot(const robot_t& robot)
-{
-    if (robot.station().id() != (int)stations::Charging)
-    {
-        throw amr_exception(
-            gaia_fmt::format("Cannot charge robot {} since it is not on a charging station waypoint", robot.id()));
-    }
-
-    if (show_outgoing_robot_messages)
-    {
-        gaia_log::app().info("BOT>>charge: robot {}.", robot.id());
-    }
-
-    // communication::publish_message(create_bot_topic(robot, c_topic_suffix_charge), c_status_empty_payload);
 }
 
 void drop_widget_at_station(const robot_t& robot)
@@ -183,8 +145,6 @@ void drop_widget_at_station(const robot_t& robot)
     {
         gaia_log::app().info("BOT>>drop_widget: robot {}.", robot.id());
     }
-
-    // communication::publish_message(create_bot_topic(robot, "drop_payload"), robot.station().sandbox_id());
 }
 
 void pickup_widget_from_station(const robot_t& robot, const gaia::amr_swarm::widget_t widget)
@@ -219,8 +179,6 @@ void pickup_widget_from_station(const robot_t& robot, const gaia::amr_swarm::wid
     {
         gaia_log::app().info("BOT>>pickup_widget: from station {} with robot {}.", robot.station().id(), robot.id());
     }
-
-    // communication::publish_message(create_bot_topic(robot, c_topic_suffix_pickup_payload), robot.station().sandbox_id());
 }
 
 void press_unpack_pallet_button()
