5c3f4b58f65f56b1fd39e5995137e082824c3575
Tobin Baker <tobin.d.baker@gmail.com>
2021-10-02 15:12:49 -0700

    enable multiple threads and stack traces

--- scratch/tobin/iterator_stress_test/iterator_stress_test.cpp
+++ scratch/tobin/iterator_stress_test/iterator_stress_test.cpp
@@ -4,6 +4,8 @@
 /////////////////////////////////////////////
 
 #include <iostream>
+#include <vector>
+#include <thread>
 
 #include "gaia/system.hpp"
 
@@ -21,15 +23,29 @@ int main()
     gaia::iterator_stress_test::my_table_t::insert_row("my_value");
     gaia::db::commit_transaction();
 
-    while (true)
+    constexpr size_t c_num_threads = 16;
+    std::vector<std::thread> threads;
+
+    for (size_t i = 0; i < c_num_threads; ++i)
     {
-        gaia::db::begin_transaction();
-        my_table_t my_table = *(my_table_t::list().begin());
-        auto my_value = my_table.my_value();
-        std::cerr << "Value of my_table.my_value: " << my_value << std::endl;
-        gaia::db::commit_transaction();
+        threads.emplace_back([i]() {
+            gaia::db::begin_session();
+            while (true)
+            {
+                gaia::db::begin_transaction();
+                my_table_t my_table = *(my_table_t::list().begin());
+                auto my_value = my_table.my_value();
+                // std::cerr << "Thread " << i << ": Value of my_table.my_value: " << my_value << std::endl;
+                gaia::db::commit_transaction();
+            }
+            gaia::db::end_session();
+        });
     }
 
+    for (auto& t : threads)
+    {
+        t.join();
+    }
     gaia::system::shutdown();
 
     std::cerr << "iterator_stress_test has shut down." << std::endl;
