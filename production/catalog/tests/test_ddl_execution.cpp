04810e6c1776a2f699dd0c3e30131360cb329b90
Tobin Baker <tobin.d.baker@gmail.com>
2021-10-07 06:06:55 -0700

    uncomment test code

--- production/catalog/tests/test_ddl_execution.cpp
+++ production/catalog/tests/test_ddl_execution.cpp
@@ -322,10 +322,8 @@ drop database hospital;
     for (const auto& ddl : ddls)
     {
         ddl::parser_t parser;
-        // ASSERT_NO_THROW(parser.parse_string(ddl));
-        parser.parse_string(ddl);
-        // ASSERT_NO_THROW(execute(parser.statements));
-        execute(parser.statements);
+        ASSERT_NO_THROW(parser.parse_string(ddl));
+        ASSERT_NO_THROW(execute(parser.statements));
     }
 }
 
