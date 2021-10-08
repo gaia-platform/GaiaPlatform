6caafa3514a0cf15d369a1ae5b04c8689a2d752f
jc-gaiaplatform <jorge@gaiaplatform.io>
2021-10-08 13:05:57 -0700

    Intializes application

---
+++ dev_tools/database-tool-ext/src/App.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import './App.css';
+
+import gaia_logo from './gaia_logo.png';
+import MainContainer from './containers/MainContainer'
+
+function App(){
+    return (
+        <div className="App">
+        <header className="App-header">
+            <img src={gaia_logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Gaia Platform Database Viewer</h1>
+        </header>
+        <MainContainer></MainContainer>
+        </div>
+    );
+}
+
+export default App;
