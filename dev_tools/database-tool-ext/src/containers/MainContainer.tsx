c3a759c33aaf6c8c150b5fa85208d0ebcbb935db
jc-gaiaplatform <jorge@gaiaplatform.io>
2021-10-08 13:04:38 -0700

    Container for table component

---
+++ dev_tools/database-tool-ext/src/containers/MainContainer.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import CollapsibleTable from '../components/Table';
+
+export default function MainContainer() {
+    return (
+        <CollapsibleTable></CollapsibleTable>
+    )
+}
