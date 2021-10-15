8b9fbbb9e5c68cdc14c258812c1420c05f54dbad
jc-gaiaplatform <jorge@gaiaplatform.io>
2021-10-15 12:08:54 -0700

    nit fixes

--- dev_tools/database-tool-ext/src/App.tsx
+++ dev_tools/database-tool-ext/src/App.tsx
@@ -4,14 +4,14 @@ import './App.css';
 import gaia_logo from './gaia_logo.png';
 import MainContainer from './containers/MainContainer'
 
-function App(){
+function App() {
     return (
         <div className="App">
-        <header className="App-header">
-            <img src={gaia_logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Gaia Platform Database Viewer</h1>
-        </header>
-        <MainContainer></MainContainer>
+            <header className="App-header">
+                <img src={gaia_logo} className="App-logo" alt="logo" />
+                <h1 className="App-title">Gaia Platform Database Viewer</h1>
+            </header>
+            <MainContainer></MainContainer>
         </div>
     );
 }
