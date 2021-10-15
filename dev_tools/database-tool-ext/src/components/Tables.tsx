c20683ed2f8173cae3b603a85253f9aa2456d387
jc-gaiaplatform <jorge@gaiaplatform.io>
2021-10-15 12:12:17 -0700

    Remodified to have a simpler UI. Each row will redirect to a new table on doubleclick.

---
+++ dev_tools/database-tool-ext/src/components/Tables.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import './components.css'
+
+// Tables function to be exported for use in MainContainer
+export default function Tables() {
+    // Hook to set initial state as placeholder names until database info is accessed.
+    const [table_names, useTable_names] = React.useState(
+        [
+            'Table Name 1',
+            'Table Name 2',
+            'Table Name 3',
+            "Table Name 4",
+            'Table Name 5',
+            'Table Name 6'
+        ]
+    )
+
+    // Returns the initial table webview that contains all of the tables in the current database.
+    return (
+        <table id="tables">
+            <thead>
+                <tr>
+                    <th>Tables</th>
+                </tr>
+            </thead>
+            <tbody>
+                {table_names.map(tname => (
+                    <tr>
+                        <td>{tname}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    )
+}
