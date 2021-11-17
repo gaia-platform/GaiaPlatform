0aa6f3f0ef7f6a1f45718920ffc790c70fef6d99
yiwen-wong <yiwen@gaiaplatform.io>
2021-11-17 13:10:02 -0800

    Fix assert condition

--- production/db/core/src/index_builder.cpp
+++ production/db/core/src/index_builder.cpp
@@ -167,7 +167,7 @@ void update_index_entry(
                 = transactions::txn_metadata_t::get_commit_ts_from_begin_ts(begin_ts);
 
             ASSERT_PRECONDITION(
-                begin_ts == transactions::is_frozen_ts(begin_ts) || transactions::txn_metadata_t::is_begin_ts(begin_ts),
+                transactions::is_frozen_ts(begin_ts) || transactions::txn_metadata_t::is_begin_ts(begin_ts),
                 "Transaction id in index key entry is not frozen or a begin timestamp!");
 
             // Index entries made by rolled back transactions or aborted transactions can be ignored,
