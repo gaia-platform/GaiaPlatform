32bb5eddb8cf247af8d6566656bf9b2f48da38b3
jc-gaiaplatform <jorge@gaiaplatform.io>
2021-12-03 09:26:57 -0800

    fixed merge conflicts

--- dev_tools/intellisense-vscode-ext/client/src/extension.ts
+++ dev_tools/intellisense-vscode-ext/client/src/extension.ts
@@ -1,53 +1,5 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { hover_info } from './hover_info';
 import {
-    workspace,
-    ExtensionContext,
-    languages,
-    Hover,
-    MarkdownString
-} from 'vscode';
-
-import {
-    LanguageClient,
-    LanguageClientOptions,
-    ServerOptions,
-    TransportKind
-} from 'vscode-languageclient/node';
-
-let client: LanguageClient;
-
-function activate(context: ExtensionContext) {
-
-    // Creates map out of hover_info keys/values.
-    const hover_info_map = new Map(Object.entries(hover_info))
-
-    // Creates provider to give the extension hover functionality.
-    languages.registerHoverProvider('ruleset', {
-        provideHover(document, position, token) {
-
-            // Finds the line that the user's cursor is hovering over.
-            const range = document.getWordRangeAtPosition(position);
-
-            // Gets the word that the cursor is hovering over given the line.
-            const word: string = document.getText(range);
-
-            // Constructs each hover based on its value in hover_info_map.
-            if (hover_info_map.has(word)) {
-
-                // Creates markdown string to be used in hover.
-                let markdown_string = new MarkdownString(`${hover_info_map.get(word)}`)
-
-                // Indicates that the markdown string is from a trusted source.
-                markdown_string.isTrusted = true;
-=======
-// const vscode = require('vscode');
-// const hover_info = require('..hover_info.json');
-=======
->>>>>>> d987c1231 (removed extra comments)
-import { hover_info } from './hover_info';
-import { 
     ExtensionContext,
     languages,
     Hover,
@@ -75,18 +27,8 @@ function activate(context: ExtensionContext) {
                 // Creates markdown string to be used in hover.
                 let markdown_string = new MarkdownString(`${hover_info_map.get(word)}`)
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-				//Indicates that the markdown string is from a trusted source.
-				markdown_string.isTrusted = true;
->>>>>>> f837722ec (markdown strings for hovers)
-=======
-                //Indicates that the markdown string is from a trusted source.
-=======
                 // Indicates that the markdown string is from a trusted source.
->>>>>>> a56a929f8 (spacig after double slashes)
                 markdown_string.isTrusted = true;
->>>>>>> c6f9fdaf5 (converted spacing back to spaces)
                 return new Hover(markdown_string);
             }
         }
