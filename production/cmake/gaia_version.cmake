c11f67af4e95c7518a848875c9d91b9272568df2
Jack De Winter <jack@gaiaplatform.io>
2021-12-03 13:34:01 -0800

    test

--- production/cmake/gaia_version.cmake
+++ production/cmake/gaia_version.cmake
@@ -5,38 +5,8 @@
 
 # TeamCity/gdev builds will not be able to produce the git hash because
 # the source are copied without the Git info.
-find_package(Git)
-# Get the latest git hash for the current branch.
-execute_process(
-  COMMAND
-    ${GIT_EXECUTABLE} rev-parse --short HEAD
-  RESULT_VARIABLE
-    STATUS
-  OUTPUT_VARIABLE
-    GIT_HEAD_HASH
-)
-
-if (NOT ${STATUS} EQUAL 0)
-  set(GIT_HEAD_HASH "not-found")
-else()
-  string(REGEX REPLACE "\n" "" GIT_HEAD_HASH "${GIT_HEAD_HASH}")
-endif()
-
-# Get the latest git hash from master merged into the current branch.
-execute_process(
-  COMMAND
-    ${GIT_EXECUTABLE} rev-parse --short origin/master
-  RESULT_VARIABLE
-    STATUS
-  OUTPUT_VARIABLE
-    GIT_MASTER_HASH
-)
-
-if (NOT ${STATUS} EQUAL 0)
-  set(GIT_MASTER_HASH "not-found")
-else()
-  string(REGEX REPLACE "\n" "" GIT_MASTER_HASH "${GIT_MASTER_HASH}")
-endif()
+set(GIT_HEAD_HASH "not-found")
+set(GIT_MASTER_HASH "not-found")
 
 # Populate the gaia_version file. Not this applies only for local builds.
 # In CI the BUILD_NUMBER is replaced directly by TeamCity before calling cmake.
