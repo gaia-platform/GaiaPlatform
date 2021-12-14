25022c1787b039d70c13f212d74fd4f33ba93544
Simone Rondelli <simone@gaiaplatform.io>
2021-12-14 23:59:42 +0100

    Improve Python

--- production/db/core/src/dump_se.py
+++ production/db/core/src/dump_se.py
@@ -11,27 +11,27 @@ begin_transaction()
 
 type = 1
 done = False
-while not(done):
+while not done:
     no_node = False
 
     print("Iterating through nodes of type: " + str(type) + "...")
     node_iter = gaia_ptr.find_first(type)
 
-    if (node_iter.is_null() == True):
+    if node_iter.is_null():
         print("No nodes of type " + str(type) + " were found!")
         no_node = True
 
-    while node_iter.is_null() != True:
+    while not node_iter.is_null():
         print_node(node_iter, False)
         node_iter = node_iter.find_next()
     print ("")
 
-    if (no_node):
+    if no_node:
         print("No nodes of type " + str(type) + " were found!")
         print("Terminating database scan!")
         done = True
 
-    type = type + 1
+    type += 1
 
 commit_transaction()
 
