161cb4f99d5eaf92bb3cde34cd6ef4fec9cc750c
Simone Rondelli <simone@gaiaplatform.io>
2021-12-21 09:56:07 -0500

    Fix hello example include path

--- production/examples/hello/build.sh
+++ production/examples/hello/build.sh
@@ -10,6 +10,7 @@
 
 gaiac -g hello.ddl -d hello -o .
 
-gaiat hello.ruleset -output hello_ruleset.cpp -- -I /usr/include/clang/10 -I /opt/gaia/include
+CLANG_INCLUDE_PATH="$(clang++-10 -print-resource-dir)/include"
+gaiat hello.ruleset -output hello_ruleset.cpp -- -I "$CLANG_INCLUDE_PATH" -I /opt/gaia/include
 
 clang++-10 hello.cpp hello_ruleset.cpp gaia_hello.cpp /usr/local/lib/libgaia.so -I /opt/gaia/include -Wl,-rpath,/usr/local/lib -lpthread -o hello
