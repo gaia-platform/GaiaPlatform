bb505f0c711a178b9884b4aed1ffb38189515ff1
Jack De Winter <jack@gaiaplatform.io>
2022-01-11 13:06:09 -0800

    test

---
+++ dev_tools/github-actions/copy_until_line.py
@@ -0,0 +1,64 @@
+#! /usr/bin/python3
+
+#############################################
+# Copyright (c) Gaia Platform LLC
+# All rights reserved.
+#############################################
+
+import sys
+import argparse
+import pathlib
+
+__DEFAULT_FILE_ENCODING = "utf-8"
+
+def __process_command_line():
+    """
+    Process the arguments on the command line.
+    """
+    parser = argparse.ArgumentParser(
+        description="Copy a file from one place to another until we hit a specified line."
+    )
+    parser.add_argument(
+        "--input-file",
+        dest="input_path",
+        action="store",
+        required=True,
+        help="File to use as input.",
+    )
+    parser.add_argument(
+        "--output-file",
+        dest="output_path",
+        action="store",
+        required=True,
+        help="File to use as output.",
+    )
+    parser.add_argument(
+        "--stop-line",
+        dest="stop_line",
+        action="store",
+        required=True,
+        help="Contents of line to stop copying at.",
+    )
+    return parser.parse_args()
+
+def process_script_action():
+    """
+    Process the posting of the message.
+    """
+    args = __process_command_line()
+
+    
+    # output_path
+    # stop-line
+    lines_in_text_file = (
+        pathlib.Path(args.input_path)
+        .read_text(encoding=__DEFAULT_FILE_ENCODING)
+        .split("\n")
+    )
+    with pathlib.Path(args.output_path).open(mode='w', encoding=__DEFAULT_FILE_ENCODING) as f:
+        for next_line in lines_in_text_file:
+            if next_line == args.stop_line:
+                break
+            f.write(next_line + "\n")
+
+sys.exit(process_script_action())
