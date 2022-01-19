56212581a0f0548e3d31f44c0abfd56011927b8d
Phillip Ovanesyan <phillipov@outlook.com>
2022-01-19 22:45:26 +0000

    Combined inserting and updating functions in the VLR procedural code; removed unused_floor_num

--- production/examples/value_linked_relationships/vlr_example.cpp
+++ production/examples/value_linked_relationships/vlr_example.cpp
@@ -18,75 +18,47 @@
 using namespace gaia::vlr_example;
 using gaia::direct_access::auto_transaction_t;
 
-// We cannot delete records (yet) from a Value-Linked Relationship
-// without first disconnecting the records by changing the value of a linked field.
-// We implicitly agree to never use int's maximum value as a floor number,
-// so to disconnect people from floors, we set their floor numbers to this value.
-const int unused_floor_num = std::numeric_limits<int>::max();
-
-void insert_floors_and_people()
+void reset_database_to_clean_slate()
 {
     auto_transaction_t txn{auto_transaction_t::no_auto_begin};
-
-    try
-    {
-        // Floors inserted with their numbers and department names.
-        floor_t::insert_row(0, "Lobby");
-        floor_t::insert_row(1, "Sales");
-        floor_t::insert_row(2, "Engineering");
-        floor_t::insert_row(3, "Admin");
-
-        // Insert people at certain floors. Bill starts at floor 0: the lobby.
-        person_t::insert_row("Bill", 0);
-        person_t::insert_row("Todd", 1);
-        person_t::insert_row("Jane", 1);
-        person_t::insert_row("John", 2);
-        person_t::insert_row("Sarah", 2);
-        person_t::insert_row("Ned", 2);
-        person_t::insert_row("Dave", 3);
-
-        txn.commit();
-    }
-    catch (gaia::db::index::unique_constraint_violation& uniqueness_violated)
-    {
-        gaia_log::app().error("{}", uniqueness_violated.what());
-    }
-}
-
-void delete_all_floors_and_people()
-{
-    auto_transaction_t txn{auto_transaction_t::no_auto_begin};
-
-    for (auto floor = *floor_t::list().begin(); floor; floor = *floor_t::list().begin())
-    {
-        auto floor_w = floor.writer();
-        // Implicitly disconnect the floor from the people by changing its number
-        // to an unused floor number.
-        floor_w.num = unused_floor_num;
-        floor_w.update_row();
-        floor.delete_row();
-    }
 
     for (auto person = *person_t::list().begin(); person; person = *person_t::list().begin())
     {
         person.delete_row();
     }
+    for (auto floor = *floor_t::list().begin(); floor; floor = *floor_t::list().begin())
+    {
+        floor.delete_row();
+    }
     txn.commit();
 }
 
-void move_around_floors()
+void vlr_example_usage()
 {
     auto_transaction_t txn{};
-    // Retrieve the "person" record corresponding to Bill and get his writer
-    // to update his record values.
-    person_t bill = *(person_t::list().where(person_expr::name == "Bill").begin());
+    // Floors inserted with their numbers and department names.
+    floor_t::insert_row(0, "Lobby");
+    floor_t::insert_row(1, "Sales");
+    floor_t::insert_row(2, "Engineering");
+    floor_t::insert_row(3, "Admin");
+    // Insert people at certain floors. Bill starts at floor 0: the lobby.
+    person_t bill = person_t::get(person_t::insert_row("Bill", 0));
+    person_t::insert_row("Todd", 1);
+    person_t::insert_row("Jane", 1);
+    person_t::insert_row("John", 2);
+    person_t::insert_row("Sarah", 2);
+    person_t::insert_row("Ned", 2);
+    person_t::insert_row("Dave", 3);
+    txn.commit();
+
+    // We need a writer to change Bill's field values.
     person_writer bill_w = bill.writer();
 
     // Move Bill up a floor three times.
     for (int i = 0; i < 3; ++i)
     {
         // Changing his floor is as easy as incrementing floor_num.
-        // With VLRs, this automatically
+        // With VLRs, this automatically reconnects Bill to the correct floor.
         bill_w.floor_num++;
         bill_w.update_row();
         txn.commit();
@@ -101,10 +73,8 @@ void move_around_floors()
 int main()
 {
     gaia::system::initialize();
-    // Delete the "floor" and "person" records to start with a clean database.
-    delete_all_floors_and_people();
+    reset_database_to_clean_slate();
 
-    insert_floors_and_people();
-    move_around_floors();
+    vlr_example_usage();
     gaia::system::shutdown();
 }
