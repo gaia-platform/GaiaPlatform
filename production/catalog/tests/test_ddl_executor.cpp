gaia-platform/GaiaPlatform

commit 3eca9d7289a0e14ce8ecb7e1e51580d532833ce8

[GAIAPLAT-1907] Allow using optional scalar value in fbs binary schema (#1247)
- Simplify `generate_fbs` and `generate_json` APIs by leaving only methods that accept `table_id` and removing all the others. This is helpful to allow the next "refactorings"
- Add `ignore_optional` flag to `generate_fbs` as a workaround to prevent the generation of optional scalar values which, as of flatbuffers 2.0.0, are not supported when reading data from JSON. https://github.com/google/flatbuffers/issues/6975.
- Update `ddl_executor_t::create_table_impl` to first create the table and then use the `table_id` to generate the `fbs`, `bfbs`, `json` and `bin`.

production/catalog/src/ddl_executor.cpp
@@ -908,26 +908,22 @@ gaia_id_t ddl_executor_t::create_table_impl(
         field_names.insert(field_name);
     }
 
-    string fbs{generate_fbs(in_context(db_name), table_name, fields)};
-    const std::vector<uint8_t> bfbs = generate_bfbs(fbs);
-    const std::vector<uint8_t> bin = generate_bin(fbs, generate_json(fields));
-
     gaia_type_t table_type
         = (fixed_type == c_invalid_gaia_type)
         ? gaia_type_t(generate_table_type(in_context(db_name), table_name))
         : fixed_type;
 
-    gaia_id_t table_id = gaia_table_t::insert_row(
-        table_name.c_str(),
-        table_type,
-        is_system,
-        bfbs,
-        bin);
-
-    gaia_log::catalog().debug("Created table '{}', type:'{}', id:'{}'", table_name, table_type, table_id);
+    gaia_table_writer table_w;
+    table_w.name = table_name.c_str();
+    table_w.type = table_type;
+    table_w.is_system = true;
+    gaia_id_t table_id = table_w.insert_row();
+    gaia_table_t gaia_table = gaia_table_t::get(table_id);
 
     // Connect the table to the database.
     gaia_database_t::get(db_id).gaia_tables().insert(table_id);
+
+    gaia_log::catalog().debug("Created table '{}', type:'{}', id:'{}'", table_name, table_type, table_id);
 
     uint16_t data_field_position = 0;
     for (const auto& field : fields)
@@ -948,7 +944,7 @@ gaia_id_t ddl_executor_t::create_table_impl(
             data_field->unique,
             data_field->optional);
         // Connect the field to the table it belongs to.
-        gaia_table_t::get(table_id).gaia_fields().insert(field_id);
+        gaia_table.gaia_fields().insert(field_id);
         // Create an unique range index for the unique field.
         if (data_field->unique)
         {
@@ -957,10 +953,24 @@ gaia_id_t ddl_executor_t::create_table_impl(
                 true,
                 static_cast<uint8_t>(index_type_t::range),
                 {field_id});
-            gaia_table_t::get(table_id).gaia_indexes().insert(index_id);
+            gaia_table.gaia_indexes().insert(index_id);
         }
         data_field_position++;
     }
+
+    // After creating the table, generates the bfbs and the serialization template.
+    // using the newly generated table_id.
+    string fbs{generate_fbs(table_id)};
+    const std::vector<uint8_t> bfbs = generate_bfbs(fbs);
+
+    bool ignore_optional = true;
+    string fbs_without_optional{generate_fbs(table_id, ignore_optional)};
+    const std::vector<uint8_t> serialization_template = generate_bin(fbs_without_optional, generate_json(table_id));
+
+    table_w = gaia_table.writer();
+    table_w.serialization_template = serialization_template;
+    table_w.binary_schema = bfbs;
+    table_w.update_row();
 
     return table_id;
 }

production/catalog/inc/fbs_generator.hpp
@@ -18,22 +18,15 @@ namespace catalog
  * Generate FlatBuffers schema (fbs) for a catalog table.
  * The given table is the root type of the generated schema.
  *
+ * @param table_id The table id for which to generate the FlatBuffers schema
+ * @param ignore_optional This is a workaround to prevent the generation of optional
+ *        scalar values which, as of flatbuffers 2.0.0, are not supported when reading
+ *        data from JSON. When generating a fbs meant for JSON template data this
+ *        value should be true, false otherwise.
+ *        See: https://github.com/google/flatbuffers/issues/6975
  * @return generated fbs string
  */
-std::string generate_fbs(gaia::common::gaia_id_t table_id);
-
-/**
- * Generate FlatBuffers schema (fbs) from parsing result of a table.
- * Before the support of complex types, we can generate a table schema from its own own definition.
- * This method has no dependency on catalog or other table definitions.
- * Note: If we begin to supoort complex types, the method will need to be updated sigificantly.
- *
- * @param db_name database name
- * @param table_name table name
- * @param fields table fields parsing result bindings
- * @return fbs schema
- */
-std::string generate_fbs(const std::string& db_name, const std::string& table_name, const ddl::field_def_list_t& fields);
+std::string generate_fbs(gaia::common::gaia_id_t table_id, bool ignore_optional = false);
 
 /**
  * Generate binary FlatBuffers schema (bfbs) in base64 encoded string format.

production/catalog/inc/json_generator.hpp
@@ -23,14 +23,6 @@ namespace catalog
 std::string generate_json(gaia::common::gaia_id_t table_id);
 
 /**
- * Generate a default data file (JSON) from parsing the definition of a table.
- *
- * @param fields table fields parsing result bindings
- * @return JSON file
- */
-std::string generate_json(const ddl::field_def_list_t& fields);
-
-/**
  * Generate serializations (bin) in base64 encoded string format.
  *
  * @param fbs flatbuffers schema

production/catalog/src/fbs_generator.cpp
@@ -42,27 +42,38 @@ static string generate_fbs_namespace(const string& db_name)
     }
 }
 
-static string generate_fbs_field(const string& name, const string& type, int count)
+static string generate_fbs_field(const string& name, const string& type, int repeated_count, bool optional)
 {
-    if (count == 1)
+    std::stringstream ss;
+    ss << name;
+
+    if (repeated_count == 1)
     {
-        return name + ":" + type;
+        ss << ":" + type;
     }
-    else if (count == 0)
+    else if (repeated_count == 0)
     {
-        return name + ":[" + type + "]";
+        ss << ":[" + type + "]";
     }
     else
     {
-        return name + ":[" + type + ":" + to_string(count) + "]";
+        ss << ":[" + type + ":" + to_string(repeated_count) + "]";
     }
+
+    // TODO this will fail for non-scalar values, and for now it's acceptable.
+    if (optional)
+    {
+        ss << "=null";
+    }
+
+    return ss.str();
 }
 
-static string generate_fbs_field(const gaia_field_t& field)
+static string generate_fbs_field(const gaia_field_t& field, bool ignore_optional = false)
 {
     string name{field.name()};
     string type{get_data_type_name(static_cast<data_type_t>(field.type()))};
-    return generate_fbs_field(name, type, field.repeated_count());
+    return generate_fbs_field(name, type, field.repeated_count(), !ignore_optional && field.optional());
 }
 
 string get_data_type_name(data_type_t data_type)
@@ -102,10 +113,9 @@ string get_data_type_name(data_type_t data_type)
     }
 }
 
-string generate_fbs(gaia_id_t table_id)
+string generate_fbs(gaia_id_t table_id, bool ignore_optional)
 {
     string fbs;
-    gaia::db::begin_transaction();
     gaia_table_t table = gaia_table_t::get(table_id);
     fbs += generate_fbs_namespace(table.database().name());
     string table_name{table.name()};
@@ -113,11 +123,10 @@ string generate_fbs(gaia_id_t table_id)
     for (gaia_id_t field_id : list_fields(table_id))
     {
         gaia_field_t field = gaia_field_t::get(field_id);
-        fbs += "\t" + generate_fbs_field(field) + ";\n";
+        fbs += "\t" + generate_fbs_field(field, ignore_optional) + ";\n";
     }
     fbs += "}\n";
     fbs += "root_type " + table_name + ";";
-    gaia::db::commit_transaction();
     return fbs;
 }
 
@@ -141,26 +150,6 @@ string generate_fbs(const string& db_name)
         fbs += "}\n\n";
     }
     gaia::db::commit_transaction();
-    return fbs;
-}
-
-string generate_fbs(const string& db_name, const string& table_name, const ddl::field_def_list_t& fields)
-{
-    string fbs = generate_fbs_namespace(db_name);
-    fbs += "table " + table_name + "{";
-    for (auto& field : fields)
-    {
-        if (field->field_type == ddl::field_type_t::reference)
-        {
-            continue;
-        }
-        const ddl::data_field_def_t* data_field = dynamic_cast<ddl::data_field_def_t*>(field.get());
-        string field_fbs = generate_fbs_field(
-            data_field->name, get_data_type_name(data_field->data_type), data_field->length);
-        fbs += field_fbs + ";";
-    }
-    fbs += "}";
-    fbs += "root_type " + table_name + ";";
     return fbs;
 }
 

production/catalog/src/json_generator.cpp
@@ -14,6 +14,8 @@
 #include "gaia_internal/catalog/gaia_catalog.h"
 #include "gaia_internal/common/retail_assert.hpp"
 
+#include "gaia_spdlog/fmt/fmt.h"
+
 using namespace std;
 using namespace gaia::common;
 
@@ -94,8 +96,6 @@ static string generate_json_field(const gaia_field_t& field)
 
 string generate_json(gaia_id_t table_id)
 {
-    gaia::db::begin_transaction();
-
     stringstream json_string_stream;
     json_string_stream << "{";
 
@@ -118,44 +118,6 @@ string generate_json(gaia_id_t table_id)
         << endl
         << "}" << endl;
 
-    gaia::db::commit_transaction();
-
-    return json_string_stream.str();
-}
-
-string generate_json(const ddl::field_def_list_t& fields)
-{
-    stringstream json_string_stream;
-    json_string_stream << "{";
-
-    bool has_output_first_field = false;
-    for (auto& field : fields)
-    {
-        if (field->field_type == ddl::field_type_t::reference)
-        {
-            continue;
-        }
-
-        if (has_output_first_field)
-        {
-            json_string_stream << ",";
-        }
-        has_output_first_field = true;
-
-        const ddl::data_field_def_t* data_field = dynamic_cast<ddl::data_field_def_t*>(field.get());
-
-        string field_json = generate_json_field(
-            data_field->name, get_data_type_default_value(data_field->data_type), data_field->length);
-
-        json_string_stream
-            << endl
-            << field_json;
-    }
-
-    json_string_stream
-        << endl
-        << "}" << endl;
-
     return json_string_stream.str();
 }
 
@@ -166,10 +128,12 @@ vector<uint8_t> generate_bin(const string& fbs, const string& json)
     flatbuffers::Parser parser(options);
 
     bool parsing_result = parser.Parse(fbs.c_str());
-    ASSERT_INVARIANT(parsing_result == true, "Invalid FlatBuffers schema!");
+    ASSERT_INVARIANT(
+        parsing_result == true, gaia_fmt::format("Invalid FlatBuffers schema: {}", parser.error_).c_str());
 
     parsing_result = parser.Parse(json.c_str());
-    ASSERT_INVARIANT(parsing_result == true, "Invalid FlatBuffers JSON!");
+    ASSERT_INVARIANT(
+        parsing_result == true, gaia_fmt::format("Invalid FlatBuffers JSON: {}", parser.error_).c_str());
 
     // Use the std::vector (begin, end) iterator constructor.
     return {

production/catalog/tests/test_fbs_generation.cpp
@@ -6,6 +6,8 @@
 #include <flatbuffers/idl.h>
 #include <gtest/gtest.h>
 
+#include "gaia/direct_access/auto_transaction.hpp"
+
 #include "gaia_internal/db/db_test_base.hpp"
 
 #include "fbs_generator.hpp"
@@ -15,6 +17,7 @@ using namespace std;
 using namespace gaia::catalog;
 using namespace gaia::common;
 using namespace gaia::db;
+using namespace gaia::direct_access;
 
 class fbs_generation_test : public db_test_base_t
 {
@@ -37,19 +40,9 @@ TEST_F(fbs_generation_test, generate_fbs_from_catalog)
     string test_table_name{"test_fbs_generation"};
 
     gaia_id_t table_id = create_table(test_table_name, test_table_fields);
+
+    auto_transaction_t txn;
     string fbs = generate_fbs(table_id);
-
-    flatbuffers::Parser fbs_parser;
-
-    EXPECT_GT(fbs.size(), 0);
-    ASSERT_TRUE(fbs_parser.Parse(fbs.c_str()));
-}
-
-TEST_F(fbs_generation_test, generate_fbs_from_table_definition)
-{
-    string test_table_name{"test_fbs_generation"};
-
-    string fbs = generate_fbs("", test_table_name, test_table_fields);
 
     flatbuffers::Parser fbs_parser;
 
@@ -61,7 +54,10 @@ TEST_F(fbs_generation_test, generate_bfbs)
 {
     string test_table_name{"test_fbs_generation"};
 
-    string fbs = generate_fbs("", test_table_name, test_table_fields);
+    gaia_id_t table_id = create_table(test_table_name, test_table_fields);
+
+    auto_transaction_t txn;
+    string fbs = generate_fbs(table_id);
     vector<uint8_t> bfbs = generate_bfbs(fbs);
 
     ASSERT_GT(bfbs.size(), 0);

production/catalog/tests/test_json_generation.cpp
@@ -6,6 +6,8 @@
 #include <flatbuffers/idl.h>
 #include <gtest/gtest.h>
 
+#include "gaia/direct_access/auto_transaction.hpp"
+
 #include "gaia_internal/db/db_test_base.hpp"
 
 #include "fbs_generator.hpp"
@@ -17,6 +19,7 @@ using namespace gaia::catalog;
 using namespace gaia::catalog::ddl;
 using namespace gaia::common;
 using namespace gaia::db;
+using namespace gaia::direct_access;
 
 class json_generation_test : public db_test_base_t
 {
@@ -64,18 +67,9 @@ TEST_F(json_generation_test, generate_json_from_catalog)
 
     gaia_id_t table_id = create_table(test_table_name, test_table_fields);
 
+    auto_transaction_t txn;
     string fbs = generate_fbs(table_id);
     string json = generate_json(table_id);
-
-    validate_through_flatbuffers_parser(fbs, json);
-}
-
-TEST_F(json_generation_test, generate_json_from_table_definition)
-{
-    string test_table_name{"test_generate_json_from_table_definition"};
-
-    string fbs = generate_fbs("", test_table_name, test_table_fields);
-    string json = generate_json(test_table_fields);
 
     validate_through_flatbuffers_parser(fbs, json);
 }
@@ -84,11 +78,13 @@ TEST_F(json_generation_test, generate_bin)
 {
     string test_table_name{"test_generate_bin"};
 
-    string fbs = generate_fbs("", test_table_name, test_table_fields);
+    gaia_id_t table_id = create_table(test_table_name, test_table_fields);
+    auto_transaction_t txn;
+    string fbs = generate_fbs(table_id);
     const vector<uint8_t> bfbs = generate_bfbs(fbs);
     ASSERT_GT(bfbs.size(), 0);
 
-    string json = generate_json(test_table_fields);
+    string json = generate_json(table_id);
     const vector<uint8_t> bin = generate_bin(fbs, json);
     ASSERT_GT(bin.size(), 0);
 

production/db/core/CMakeLists.txt
@@ -285,3 +285,8 @@ add_gtest(test_catalog_core
   tests/test_catalog_core.cpp
   "${FLATBUFFERS_INC};${GAIA_DB_CORE_TEST_INCLUDES};${GAIA_INC}"
   "gaia_common;gaia_db_client;gaia_catalog")
+
+add_gtest(test_optional_scalars
+  "tests/test_optional_scalars.cpp"
+  "${GAIA_DB_CORE_TEST_INCLUDES};"
+  "rt;gaia_system;gaia_db_catalog_test")

commit a51b96f17a20b75dc9269659fe8e78340bd9de6b

Fix build failure due to all tables marked as system tables (#1252)
- Fixes a bug introduced in 3eca9d7289a0e14ce8ecb7e1e51580d532833ce8 that would mark all the tables (including user-defined ones) as system tables. This leads to the translation engine not seeing any table because it skips the system tables.
- Add regression test.
- Note that the build is still broken due to a bug introduced in 4532e58191dcae17a9c189e064617fd9310052cc (it seems so at least)

production/catalog/src/ddl_executor.cpp
@@ -916,7 +916,7 @@ gaia_id_t ddl_executor_t::create_table_impl(
     gaia_table_writer table_w;
     table_w.name = table_name.c_str();
     table_w.type = table_type;
-    table_w.is_system = true;
+    table_w.is_system = is_system;
     gaia_id_t table_id = table_w.insert_row();
     gaia_table_t gaia_table = gaia_table_t::get(table_id);
 

production/catalog/tests/test_ddl_executor.cpp
@@ -108,6 +108,26 @@ TEST_F(ddl_executor_test, create_table)
     ASSERT_THROW(create_table(test_db_name, test_table_name, fields), table_already_exists);
 }
 
+TEST_F(ddl_executor_test, system_tables)
+{
+    string test_table_name{"create_table_test"};
+    ddl::field_def_list_t fields;
+    gaia_id_t table_id = create_table(test_table_name, fields);
+
+    auto_transaction_t txn;
+
+    ASSERT_FALSE(gaia_table_t::get(table_id).is_system());
+
+    for (gaia_table_t gaia_table : gaia_table_t::list())
+    {
+        if (strcmp(c_catalog_db_name.c_str(), gaia_table.database().name()) == 0
+            || strcmp(c_event_log_db_name.c_str(), gaia_table.database().name()) == 0)
+        {
+            ASSERT_TRUE(gaia_table.is_system());
+        }
+    }
+}
+
 TEST_F(ddl_executor_test, create_existing_table)
 {
     string test_table_name{"create_existing_table"};