3a551171e556413b49ed7c184d6230558c2ebfa4
Jack De Winter <jack@gaiaplatform.io>
2022-02-07 13:13:00 -0800

    test

--- dev_tools/sdk/test/build_sdk_samples.sh
+++ dev_tools/sdk/test/build_sdk_samples.sh
@@ -6,24 +6,32 @@
 #############################################
 
 # Exit the script as soon as any line fails.
+echo "Into script."
 set -e
 
 # Start the db server.
+echo "reset data store."
 gaia_db_server --reset-data-store &
+echo "sleep."
 sleep 1
 
 # Make incubator example.
+echo "Create clean inc"
 rm -rf ./incubator
 cp -r /opt/gaia/examples/incubator .
 pushd incubator
+echo "Make build"
 mkdir build
 pushd build
+echo "CMake"
 cmake ..
+echo "Make"
 make
 popd
 popd
 
 # Make and execute Hello World example.
+echo "Create clean hello"
 rm -rf ./hello
 cp -r /opt/gaia/examples/hello .
 pushd hello
