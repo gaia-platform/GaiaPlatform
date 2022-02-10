00856cad36b4b8434b4d98ce0f226254ddd3537c
Jack De Winter <jack@gaiaplatform.io>
2022-02-10 12:46:32 -0800

    test

--- production/examples/hello/build.sh
+++ production/examples/hello/build.sh
@@ -8,16 +8,10 @@
 # or at https://opensource.org/licenses/MIT.
 ###################################################
 
-echo "GAIAC"
 gaiac -g hello.ddl -d hello -o .
 
-echo "INCLUDE"
 CLANG_INCLUDE_PATH="$(clang++-10 -print-resource-dir)/include"
 
-echo "GAIAT"
 gaiat hello.ruleset -output hello_ruleset.cpp -- -I "$CLANG_INCLUDE_PATH" -I /opt/gaia/include
 
-echo "CLANG"
 clang++-10 hello.cpp hello_ruleset.cpp gaia_hello.cpp /usr/local/lib/libgaia.so -I /opt/gaia/include -Wl,-rpath,/usr/local/lib -lpthread -o hello
-
-echo "DONE"
