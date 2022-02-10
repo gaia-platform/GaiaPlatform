bef06bc2387cb8acdb352fc386aebac7509cff4e
yiwen-wong <67293355+yiwen-wong@users.noreply.github.com>
2022-02-09 20:11:24 -0800

    [GAIAPLAT-1995] Transaction conflicts cause server segmentation fault (#1327)
    * [GAIAPLAT-1995] Transaction conflicts cause server segmentation fault
    
    * Added comments about bounds and removed some redundant argument passing.
    
    * renamed to c_unbound_index_key
    
    * capitalize msg
    
    Co-authored-by: yiwen-wong <yiwen@gaiaplatform.io>

--- production/db/index/src/range_index.cpp
+++ production/db/index/src/range_index.cpp
@@ -55,10 +55,7 @@ std::pair<range_index_iterator_t, range_index_iterator_t> range_index_t::equal_r
 
 std::shared_ptr<common::iterators::generator_t<index_record_t>> range_index_t::equal_range_generator(gaia_txn_id_t txn_id, const index_key_t& key)
 {
-    std::lock_guard<std::recursive_mutex> lock(m_index_lock);
-    auto first = m_data.find(key);
-    auto last = (first == m_data.cend()) ? m_data.cend() : m_data.upper_bound(key);
-    return std::make_shared<index_generator_t<range_type_t>>(get_lock(), first, last, txn_id);
+    return std::make_shared<index_generator_t<range_type_t>>(get_lock(), m_data, key, key, txn_id);
 }
 
 template <>
@@ -67,6 +64,15 @@ index_writer_guard_t<range_type_t>::equal_range(const index_key_t& key)
 {
     auto first = m_data.find(key);
     auto last = (first == m_data.end()) ? m_data.end() : m_data.upper_bound(key);
+    return {first, last};
+}
+
+template <>
+std::pair<range_type_t::const_iterator, range_type_t::const_iterator>
+index_generator_t<range_type_t>::range() const
+{
+    range_type_t::const_iterator first = (m_begin_key == c_unbound_index_key) ? m_data.cbegin() : m_data.find(m_begin_key);
+    range_type_t::const_iterator last = (first == m_data.cend() || m_end_key == c_unbound_index_key) ? m_data.cend() : m_data.upper_bound(m_end_key);
     return {first, last};
 }
 
