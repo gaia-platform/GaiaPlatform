89dc64428e60b68793432b77625d76a90042db37
yiwen-wong <yiwen@gaiaplatform.io>
2022-02-14 10:19:37 -0800

    rename value->value_accessor

--- production/direct_access/tests/test_expressions_no_dac.cpp
+++ production/direct_access/tests/test_expressions_no_dac.cpp
@@ -22,58 +22,58 @@ TEST_F(test_expressions_no_dac, expr_add)
 {
 
     int64_t i64 = 500;
-    auto i64_value = value_accessor_t<context_t, int64_t>(i64);
+    auto i64_value_accessor = value_accessor_t<context_t, int64_t>(i64);
 
-    auto add_expression = i64_value + 100;
+    auto add_expression = i64_value_accessor + 100;
     ASSERT_EQ(add_expression(g_bind), i64 + 100);
 
     // yoda
-    auto add_expression2 = 100 + i64_value;
+    auto add_expression2 = 100 + i64_value_accessor;
     ASSERT_EQ(add_expression2(g_bind), 100 + i64);
 }
 
 TEST_F(test_expressions_no_dac, expr_sub)
 {
     int64_t i64 = 500;
-    auto i64_value = value_accessor_t<context_t, int64_t>(i64);
+    auto i64_value_accessor = value_accessor_t<context_t, int64_t>(i64);
 
-    auto sub_expression = i64_value - 100;
+    auto sub_expression = i64_value_accessor - 100;
     ASSERT_EQ(sub_expression(g_bind), i64 - 100);
 
-    auto sub_expression2 = 100 - i64_value;
+    auto sub_expression2 = 100 - i64_value_accessor;
     ASSERT_EQ(sub_expression2(g_bind), 100 - i64);
 }
 
 TEST_F(test_expressions_no_dac, expr_mul)
 {
     int64_t i64 = 500;
-    auto i64_value = value_accessor_t<context_t, int64_t>(i64);
+    auto i64_value_accessor = value_accessor_t<context_t, int64_t>(i64);
 
-    auto mul_expression = i64_value * 2;
+    auto mul_expression = i64_value_accessor * 2;
     ASSERT_EQ(mul_expression(g_bind), i64 * 2);
 
-    auto mul_expression2 = 0.25 * i64_value;
+    auto mul_expression2 = 0.25 * i64_value_accessor;
     ASSERT_EQ(mul_expression2(g_bind), 0.25 * i64);
 }
 
 TEST_F(test_expressions_no_dac, expr_div)
 {
     int64_t i64 = 500;
-    auto i64_value = value_accessor_t<context_t, int64_t>(i64);
+    auto i64_value_accessor = value_accessor_t<context_t, int64_t>(i64);
 
-    auto div_expression = i64_value / 2;
+    auto div_expression = i64_value_accessor / 2;
     ASSERT_EQ(div_expression(g_bind), i64 / 2);
 
-    auto div_expression2 = 2 / i64_value;
+    auto div_expression2 = 2 / i64_value_accessor;
     ASSERT_EQ(div_expression2(g_bind), 2 / i64);
 }
 
 TEST_F(test_expressions_no_dac, expr_neg)
 {
     int64_t i64 = 500;
-    auto i64_value = value_accessor_t<context_t, int64_t>(i64);
+    auto i64_value_accessor = value_accessor_t<context_t, int64_t>(i64);
 
-    auto neg_expression = -i64_value;
+    auto neg_expression = -i64_value_accessor;
     ASSERT_EQ(neg_expression(g_bind), -i64);
 }
 
