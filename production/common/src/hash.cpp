8c883800790140e4eadbcae10b63dad541c13cad
Wayne Warren <wayne@warrenfamilyhome.com>
2022-02-15 15:46:05 -0800

    simplified some NOLINT directives

--- production/common/src/hash.cpp
+++ production/common/src/hash.cpp
@@ -21,10 +21,10 @@ namespace hash
 // https://blog.regehr.org/archives/1063
 inline uint32_t rotl32(uint32_t x, uint32_t n)
 {
-    // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
+    // NOLINTBEGIN(cppcoreguidelines-avoid-magic-numbers)
     ASSERT_PRECONDITION(n < 32, "Out of range rotation number.");
-    // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
     return (x << n) | (x >> (-n & 31));
+    // NOLINTEND(cppcoreguidelines-avoid-magic-numbers)
 }
 
 // Compute murmur3 32 bit hash for the key.
@@ -48,16 +48,15 @@ uint32_t murmur3_32(const void* key, size_t len)
         uint32_t k1;
         std::memcpy(&k1, (blocks + i), sizeof(k1));
 
+        // NOLINTBEGIN(cppcoreguidelines-avoid-magic-numbers)
         k1 *= c1;
-        // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
         k1 = rotl32(k1, 15);
         k1 *= c2;
 
         h1 ^= k1;
-        // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
         h1 = rotl32(h1, 13);
-        // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
         h1 = h1 * 5 + 0xe6546b64;
+        // NOLINTEND(cppcoreguidelines-avoid-magic-numbers)
     }
 
     //----------
@@ -67,38 +66,34 @@ uint32_t murmur3_32(const void* key, size_t len)
 
     uint32_t k1 = 0;
 
+    // NOLINTBEGIN(cppcoreguidelines-avoid-magic-numbers)
     switch (len & 3)
     {
     case 3:
-        // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
         k1 ^= tail[2] << 16;
     case 2:
-        // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
         k1 ^= tail[1] << 8;
     case 1:
         k1 ^= tail[0];
         k1 *= c1;
-        // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
         k1 = rotl32(k1, 15);
         k1 *= c2;
         h1 ^= k1;
     };
+    // NOLINTEND(cppcoreguidelines-avoid-magic-numbers)
 
     //----------
     // finalization
 
     h1 ^= len;
 
-    // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
+    // NOLINTBEGIN(cppcoreguidelines-avoid-magic-numbers)
     h1 ^= h1 >> 16;
-    // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
     h1 *= 0x85ebca6b;
-    // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
     h1 ^= h1 >> 13;
-    // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
     h1 *= 0xc2b2ae35;
-    // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
     h1 ^= h1 >> 16;
+    // NOLINTEND(cppcoreguidelines-avoid-magic-numbers)
 
     return h1;
 }
@@ -113,14 +108,13 @@ inline uint64_t rotl64(uint64_t x, int8_t r)
 
 inline uint64_t fmix64(uint64_t k)
 {
-    // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
+    // NOLINTBEGIN(cppcoreguidelines-avoid-magic-numbers)
     k ^= k >> 33;
     k *= BIG_CONSTANT(0xff51afd7ed558ccd);
-    // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
     k ^= k >> 33;
     k *= BIG_CONSTANT(0xc4ceb9fe1a85ec53);
-    // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
     k ^= k >> 33;
+    // NOLINTEND(cppcoreguidelines-avoid-magic-numbers)
 
     return k;
 }
@@ -143,6 +137,7 @@ void murmur3_128(const void* key, const size_t len, void* out)
 
     auto blocks = reinterpret_cast<const uint64_t*>(data);
 
+    // NOLINTBEGIN(cppcoreguidelines-avoid-magic-numbers)
     for (size_t i = 0; i < nblocks; i++)
     {
         uint64_t k1;
@@ -151,29 +146,24 @@ void murmur3_128(const void* key, const size_t len, void* out)
         memcpy(&k2, (blocks + i * 2 + 1), sizeof(k2));
 
         k1 *= c1;
-        // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
-        k1 = rotl64(k1, 31); // rotl64(k1, 31);
+        k1 = rotl64(k1, 31);
         k1 *= c2;
         h1 ^= k1;
 
-        // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
         h1 = rotl64(h1, 27);
         h1 += h2;
-        // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
         h1 = h1 * 5 + 0x52dce729;
 
         k2 *= c2;
-        // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
         k2 = rotl64(k2, 33);
         k2 *= c1;
         h2 ^= k2;
 
-        // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
         h2 = rotl64(h2, 31);
         h2 += h1;
-        // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
         h2 = h2 * 5 + 0x38495ab5;
     }
+    // NOLINTEND(cppcoreguidelines-avoid-magic-numbers)
 
     //----------
     // tail
@@ -183,66 +173,50 @@ void murmur3_128(const void* key, const size_t len, void* out)
     uint64_t k1 = 0;
     uint64_t k2 = 0;
 
-    // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
+    // NOLINTBEGIN(cppcoreguidelines-avoid-magic-numbers)
     switch (len & 15)
     {
     case 15:
-        // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
         k2 ^= static_cast<uint64_t>(tail[14]) << 48;
     case 14:
-        // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
         k2 ^= static_cast<uint64_t>(tail[13]) << 40;
     case 13:
-        // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
         k2 ^= static_cast<uint64_t>(tail[12]) << 32;
     case 12:
-        // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
         k2 ^= static_cast<uint64_t>(tail[11]) << 24;
     case 11:
-        // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
         k2 ^= static_cast<uint64_t>(tail[10]) << 16;
     case 10:
-        // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
         k2 ^= static_cast<uint64_t>(tail[9]) << 8;
     case 9:
-        // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
         k2 ^= static_cast<uint64_t>(tail[8]) << 0;
         k2 *= c2;
-        // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
         k2 = rotl64(k2, 33);
         k2 *= c1;
         h2 ^= k2;
 
     case 8:
-        // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
         k1 ^= static_cast<uint64_t>(tail[7]) << 56;
     case 7:
-        // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
         k1 ^= static_cast<uint64_t>(tail[6]) << 48;
     case 6:
-        // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
         k1 ^= static_cast<uint64_t>(tail[5]) << 40;
     case 5:
-        // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
         k1 ^= static_cast<uint64_t>(tail[4]) << 32;
     case 4:
-        // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
         k1 ^= static_cast<uint64_t>(tail[3]) << 24;
     case 3:
-        // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
         k1 ^= static_cast<uint64_t>(tail[2]) << 16;
     case 2:
-        // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
         k1 ^= static_cast<uint64_t>(tail[1]) << 8;
     case 1:
-        // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
         k1 ^= static_cast<uint64_t>(tail[0]) << 0;
         k1 *= c1;
-        // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
         k1 = rotl64(k1, 31);
         k1 *= c2;
         h1 ^= k1;
     };
+    // NOLINTEND(cppcoreguidelines-avoid-magic-numbers)
 
     //----------
     // finalization
