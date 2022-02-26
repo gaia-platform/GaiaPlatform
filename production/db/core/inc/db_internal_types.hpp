0ffed8edbdc63bae7e8c5ec3bd30faa96c5b1a8d
Tobin Baker <tobin.d.baker@gmail.com>
2022-02-25 16:38:13 -0800

    Address review feedback

--- production/db/core/inc/db_internal_types.hpp
+++ production/db/core/inc/db_internal_types.hpp
@@ -119,8 +119,9 @@ struct hash_node_t
 struct log_record_t
 {
     gaia_locator_t locator;
-    // Locators are now 4 bytes, so we need to add 4 bytes of padding to keep
-    // total size at 16 bytes.
+    // We need 4 bytes of padding to maintain total size at 16 bytes.
+    // (We place the padding here to align the two offsets on an 8-byte
+    // boundary, in case we need to modify them both atomically in the future.)
     uint32_t reserved;
     gaia_offset_t old_offset;
     gaia_offset_t new_offset;
