791dd688a9111a99c932ff68d5a855127adde60e
Dax Hawkins <dax@gaiaplatform.io>
2022-03-21 23:09:33 -0700

    Fix lint errors

--- dev_tools/github-actions/post_build_inside_container.sh
+++ dev_tools/github-actions/post_build_inside_container.sh
@@ -118,10 +118,10 @@ if [ "$ACTION_NAME" == "unit_tests" ] ; then
     echo "Executing unit tests."
 
     # Enable FDW Tests
-    cd /build/production/sql
+    cd /build/production/sql || exit
     make install
     service postgresql restart
-    cd /build/production
+    cd /build/production || exit
 
     if ! ctest --output-on-failure 2>&1 | tee /build/output/ctest.log; then
         complete_process 1 "Unit tests failed to complete successfully."
