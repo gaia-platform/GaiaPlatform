f22799832faae852b93735de91d955ebc5c6183b
Laurentiu Cristofor <laurentiu.cristofor@outlook.com>
2022-03-28 14:27:35 -0700

    Clear local indexes on txn cleanup and add comments (#1437)
    Co-authored-by: Laurentiu Cristofor <laurentiu@gaiaplatform.io>

--- production/db/query_processor/src/qp_operator.cpp
+++ production/db/query_processor/src/qp_operator.cpp
@@ -32,14 +32,19 @@ gaia_txn_id_t db_client_proxy_t::get_current_txn_id()
 
 void db_client_proxy_t::update_local_indexes()
 {
+    // Update local indexes with our transaction's changes,
+    // using the transaction log.
     bool allow_create_empty = true;
     txn_log_t* txn_log = gaia::db::get_txn_log();
     index::index_builder_t::update_indexes_from_txn_log(
         txn_log,
-        client_t::s_last_index_processed_log,
+        client_t::s_last_index_processed_log_count,
         client_t::s_session_options.skip_catalog_integrity_check,
         allow_create_empty);
-    client_t::s_last_index_processed_log = txn_log->record_count;
+
+    // Update our log processing watermark, so that future calls
+    // will not reprocess the log records that we already processed so far.
+    client_t::s_last_index_processed_log_count = txn_log->record_count;
 }
 
 } // namespace query_processor
