1b6151b8a1366a9e212088e10f912ca477539bfe
Dax Hawkins <57684232+daxhaw@users.noreply.github.com>
2022-04-14 12:11:49 -0700

    Add rule performance tests (#1471)
    * Add rules performance tests

--- production/inc/gaia_internal/db/db_test_base.hpp
+++ production/inc/gaia_internal/db/db_test_base.hpp
@@ -54,7 +54,17 @@ protected:
 
     static void SetUpTestSuite()
     {
-        gaia_log::initialize({});
+        SetUpTestSuite({});
+    }
+
+    // Test suites can customize logger behavior by passing in a logger configuration file.
+    // This must be done in a test specific SetupTestSuite function which then must call
+    // db_test_base_t::SetupTestSuite with the logger file path.  If the test does not provide
+    // its own implementation of SetupTestSuite then default logging with an "empty" configuration
+    // above.
+    static void SetUpTestSuite(const std::string& log_conf_path)
+    {
+        gaia_log::initialize(log_conf_path);
 
         server_instance_config_t conf = server_instance_config_t::get_new_instance_config();
         conf.skip_catalog_integrity_check = true;
