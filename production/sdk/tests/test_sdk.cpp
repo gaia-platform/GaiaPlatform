8cb1e3ca6b2787586328e6f6d2adf17d247dc59d
Simone Rondelli <simone@gaiaplatform.io>
2022-05-02 23:47:56 +0200

    Ctest no longer run and kill gaia_db_server

--- production/sdk/tests/test_sdk.cpp
+++ production/sdk/tests/test_sdk.cpp
@@ -29,14 +29,30 @@ using namespace gaia::db::triggers;
 bool g_initialize_rules_called = false;
 std::atomic_bool g_rule_1_called = false;
 
+constexpr size_t c_wait_server_millis = 100;
+
 // Smoke tests public APIs, to ensure they work as expected and the symbols
 // are exported (https://github.com/gaia-platform/GaiaPlatform/pull/397);
 class sdk_test : public ::testing::Test
 {
 protected:
+    static void SetUpTestSuite()
+    {
+        // Starts the server in a "public way".
+        system("../db/core/gaia_db_server --persistence disabled &");
+
+        // Wait for the server.
+        std::this_thread::sleep_for(std::chrono::milliseconds(c_wait_server_millis));
+    }
+
+    static void TearDownTestSuite()
+    {
+        system("pkill -f -KILL gaia_db_server ");
+    }
+
     void SetUp() override
     {
-        // Load the schema in the database in a "public way"
+        // Load the schema in the database in a "public way".
         system("../catalog/gaiac/gaiac addr_book.ddl");
         gaia::system::initialize("./gaia.conf", "./gaia_log.conf");
     }
