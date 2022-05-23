e7767d000ed83d594f52926b564b192edb8876d9
Laurentiu Cristofor <laurentiu@gaiaplatform.io>
2022-05-23 13:02:30 -0700

    Fix SQL and DDL files

--- production/fdw/tests/test_fdw.sh
+++ production/fdw/tests/test_fdw.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
-#############################################
+###################################################
 # Copyright (c) Gaia Platform LLC
-# All rights reserved.
-#############################################
+#
+# Use of this source code is governed by the MIT
+# license that can be found in the LICENSE.txt file
+# or at https://opensource.org/licenses/MIT.
+###################################################
 
 set -euo pipefail
 IFS=$'\n\t'
